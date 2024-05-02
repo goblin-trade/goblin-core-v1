b5b896b032335529f10a40da1df9fa49da39c035
rollchad <138987133+rollchad@users.noreply.github.com>
2024-05-02 11:28:50 +0200

    feat: Factory contract boilerplate

---
+++ crates/goblin-factory/src/lib.rs
@@ -0,0 +1,36 @@
+// Allow `cargo stylus export-abi` to generate a main function.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+
+/// Use an efficient WASM allocator.
+#[global_allocator]
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
+
+sol_storage! {
+    #[entrypoint]
+    pub struct Counter {
+        uint256 number;
+    }
+}
+
+/// Declare that `Counter` is a contract with the following external methods.
+#[external]
+impl Counter {
+    /// Gets the number from storage.
+    pub fn number(&self) -> U256 {
+        self.number.get()
+    }
+
+    /// Sets a number in storage to a user-specified value.
+    pub fn set_number(&mut self, new_number: U256) {
+        self.number.set(new_number);
+    }
+
+    /// Increments `number` and updates its value in storage.
+    pub fn increment(&mut self) {
+        let number = self.number.get();
+        self.set_number(number + U256::from(1));
+    }
+}
