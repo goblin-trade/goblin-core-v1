d004e9b53eb1f9cfda52a49d9c5326ac661fda7b
rollchad <138987133+rollchad@users.noreply.github.com>
2024-05-03 09:31:54 +0200

    feat: Deploy with create2
    TODO activate

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ cargo stylus check --wasm-file-path ./target/wasm32-unknown-unknown/release/gobl
 
 cargo stylus deploy --private-key-path ./.localnet.key --endpoint http://localhost:8547
 
+cargo stylus deploy --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
+
 cast send 0xA6E41fFD769491a42A6e5Ce453259b93983a22EF 0xe50c000000000000 --rpc-url 'http://localhost:8547' --private-key $PRIVATE_KEY
 
 
@@ -27,3 +29,17 @@ cargo stylus trace --tx 0x89f684ddda3b525ce3f1bfb2ef47d99a4a382ba89901ee126b152b
 - Structs are encoded in big endian, with the first struct element at 0th index.
 
 - Use big endian to match EVM's convention.
+
+# Goblin factory
+
+```sh
+cargo build --target wasm32-unknown-unknown --release
+
+cargo stylus deploy --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
+
+# works once- something got deployed
+cast send 0xF5FfD11A55AFD39377411Ab9856474D2a7Cb697e "initializeMarket()" --rpc-url 'http://localhost:8547' --private-key $PRIVATE_KEY
+
+# activate
+cargo stylus deploy --mode activate-only --activate-program-address 0xde1718dae23f8f1fd058279853fbb4fa11dc167e --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
+```
--- crates/goblin-factory/src/lib.rs
+++ crates/goblin-factory/src/lib.rs
@@ -1,36 +1,53 @@
 // Allow `cargo stylus export-abi` to generate a main function.
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+// main function is needed for tests, else it gives a linker error
+#![cfg_attr(all(not(feature = "export-abi"), not(test)), no_main)]
+
 extern crate alloc;
 
 /// Use an efficient WASM allocator.
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use stylus_sdk::{alloy_primitives::{B256, U256}, console, deploy::RawDeploy, prelude::*};
 
 sol_storage! {
     #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+    pub struct GoblinFactory {}
+}
+
+#[external]
+impl GoblinFactory {
+    pub fn initialize_market(&mut self) -> Result<(), Vec<u8>> {
+        // random salt
+        let salt_val: [u8; 32] = [
+            33, 195, 93, 190, 27, 52, 74, 36, 136, 207, 51, 33, 214, 206, 84, 47, 142, 159, 48, 85, 68,
+            255, 9, 228, 153, 58, 98, 49, 154, 73, 124, 31,
+        ];
+        let salt = B256::new(salt_val);
+
+        let contract_bytes = include_bytes!("./goblin_market.wasm");
+
+        // Read from .wasm file
+        // let creation_code: [u8; 1] = [1];
+
+        // ETH sent to contract
+        let endowment = U256::from(0);
+
+        let res = unsafe { RawDeploy::new().salt(salt).deploy(contract_bytes, endowment)? };
+
+        console!("deployed at {:?}", res);
+        Ok(())
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
-#[external]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+    #[test]
+    fn read_wasm_bytes() {
+        // println!("ok");
+        let bytes = include_bytes!("./goblin_market.wasm");
+        println!("bytes {:?}", bytes);
     }
 }
