6cc7d9d63f35674a97f3e09d7b25b7e6e5b5b924
rollchad <138987133+rollchad@users.noreply.github.com>
2024-05-10 08:52:31 +0200

    refactor: Cleanup code

--- README.md
+++ README.md
@@ -33,29 +33,13 @@ cargo stylus trace --tx 0x89f684ddda3b525ce3f1bfb2ef47d99a4a382ba89901ee126b152b
 # Goblin factory
 
 ```sh
+# Prepare goblin-market tx_data
+cargo stylus deploy --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --dry-run --output-tx-data-to-dir ./crates/goblin-factory/src --mode deploy-only --private-key-path ./.localnet.key --endpoint http://localhost:8547
+
 cargo build --target wasm32-unknown-unknown --release
 
 cargo stylus deploy --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_factory.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
 
 # works once- something got deployed. But no code
-cast send 0xda52b25ddB0e3B9CC393b0690Ac62245Ac772527 "initializeMarket()" --rpc-url 'http://localhost:8547' --private-key $PRIVATE_KEY --gas-limit 10000000
-
-
-# activate- no code, nothing to activate
-cargo stylus deploy --mode activate-only --activate-program-address 0xde1718dae23f8f1fd058279853fbb4fa11dc167e --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
-```
-
-## Does contract hold data before activation?
-
-Yes, code is present.
-
-```sh
-# deploy and activate- 2 transactions
-cargo stylus deploy --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
-
-# deploy only- just contract creation transaction. But code is present
-cargo stylus deploy --mode deploy-only --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
-
-# activate- broken but not needed
-cargo stylus deploy --mode activate-only --activate-program-address 0xCe5303b8e8BFCa9d1857976F300fb29928522c6F --wasm-file-path ./target/wasm32-unknown-unknown/release/goblin_market.wasm --private-key-path ./.localnet.key --endpoint http://localhost:8547
+cast send 0xA6E41fFD769491a42A6e5Ce453259b93983a22EF "initializeMarket()" --rpc-url 'http://localhost:8547' --private-key $PRIVATE_KEY --gas-limit 10000000
 ```
--- crates/goblin-factory/src/lib.rs
+++ crates/goblin-factory/src/lib.rs
@@ -22,12 +22,12 @@ impl GoblinFactory {
     pub fn initialize_market(&mut self) -> Result<(), Vec<u8>> {
         // random salt
         let salt = B256::default();
-        let contract_bytes = include_bytes!("./goblin_market_bytes");
+        let contract_bytes = include_bytes!("./deployment_tx_data");
 
         console!("this address {:?}", contract::address());
 
         // expected address is correct
-        let expected_address = get_create2(contract::address(), salt, contract_bytes);
+        let expected_address = get_create2_address(contract::address(), salt, contract_bytes);
         console!("expected address {:?}", expected_address);
 
         // ETH sent to contract
@@ -46,12 +46,8 @@ impl GoblinFactory {
     }
 }
 
-fn get_create2(from: Address, salt: B256, init_code: &[u8]) -> Address {
-
-    // Address::default()
+fn get_create2_address(from: Address, salt: B256, init_code: &[u8]) -> Address {
     let init_code_hash = keccak256(init_code);
-
-    // Address::default()
 
     let mut bytes = Vec::with_capacity(1 + 20 + salt.len() + init_code_hash.len());
     bytes.push(0xff);
@@ -68,102 +64,3 @@ fn get_create2(from: Address, salt: B256, init_code: &[u8]) -> Address {
 
     address
 }
-
-#[cfg(test)]
-mod test {
-    use ethers::{
-        types::Address,
-        utils::{get_create2_address, keccak256},
-    };
-    use std::str::FromStr;
-    use stylus_sdk::alloy_primitives::{address, B256};
-
-    #[test]
-    fn read_wasm_bytes() {
-        let bytes = include_bytes!("./goblin_market.wasm");
-
-        let hex_bytes = hex::encode(bytes);
-        println!("bytes {:?}", hex_bytes);
-    }
-
-    #[test]
-    fn read_tx_data_bytes() {
-        let bytes = include_bytes!("./goblin_market_bytes");
-
-        let hex_bytes = hex::encode(bytes);
-        println!("bytes {:?}", hex_bytes);
-    }
-
-    #[test]
-    fn get_deployed_address() {
-        // 0xff16c0c231f5d3fd55d4b8e1373885218d1ccd4d
-        let bytes = include_bytes!("./deployment_tx_data");
-
-        // 0xe0830031474b01dc77eade766d15e5af018bf941
-        // let bytes = include_bytes!("./goblin_market.wasm");
-
-        // let salt = B256::default();
-        let salt = B256::new([0u8; 32]);
-
-        let this_address = Address::from_str("0x525c2aBA45F66987217323E8a05EA400C65D06DC").unwrap();
-
-        let create2_address = get_create2_address(this_address, salt, bytes);
-
-        println!("address {:?}", create2_address);
-    }
-
-
-}
-
-#[cfg(test)]
-mod test_alloy {
-    use stylus_sdk::alloy_primitives::{address, Address, FixedBytes, B256};
-
-    fn keccak256(bytes: &[u8]) -> FixedBytes<32> {
-        use tiny_keccak::{Hasher, Keccak};
-
-        let mut output = [0u8; 32];
-        let mut hasher = Keccak::v256();
-        hasher.update(bytes);
-        hasher.finalize(&mut output);
-        output.into()
-    }
-
-    fn get_create2(from: Address, salt: B256, init_code: &[u8]) -> Address {
-
-        // Address::default()
-        let init_code_hash = keccak256(init_code);
-
-        // Address::default()
-
-        let mut bytes = Vec::with_capacity(1 + 20 + salt.len() + init_code_hash.len());
-        bytes.push(0xff);
-        bytes.extend_from_slice(from.as_slice());
-        bytes.extend_from_slice(salt.as_slice());
-        bytes.extend_from_slice(init_code_hash.as_slice());
-
-        let hash = keccak256(bytes.as_slice());
-
-        let mut address_bytes = [0u8; 20];
-        address_bytes.copy_from_slice(&hash[12..]);
-
-        let address = Address::from_slice(&address_bytes);
-
-        address
-    }
-
-    #[test]
-    fn get_deployed_address_native() {
-        let bytes = include_bytes!("./goblin_market_bytes");
-        let salt = B256::new([0u8; 32]);
-
-        let this_address = address!("F5FfD11A55AFD39377411Ab9856474D2a7Cb697e");
-
-        let create2_address = get_create2(this_address, salt, bytes);
-
-        // 0xff16c0c231f5d3fd55d4b8e1373885218d1ccd4d- matches the result from ethers
-        println!("address {:?}", create2_address);
-    }
-}
-
-
