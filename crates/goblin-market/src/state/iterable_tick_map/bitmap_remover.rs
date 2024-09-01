e3af757dab6e80cbf5c9c157ebf831e0e7e5344b
rollchad <138987133+rollchad@users.noreply.github.com>
2024-09-01 16:04:21 +0200

    test: Bitmap remover unit tests

--- crates/goblin-market/src/state/iterable_tick_map/bitmap_remover.rs
+++ crates/goblin-market/src/state/iterable_tick_map/bitmap_remover.rs
@@ -62,3 +62,209 @@ impl BitmapRemover {
         bitmap.clear(&order_id.resting_order_index);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        quantities::{Ticks, WrapperU64},
+        state::{BitmapGroup, OrderId, RestingOrderIndex, SlotActions, SlotStorage},
+    };
+
+    use super::BitmapRemover;
+
+    #[test]
+    fn deactivate_on_blank_bitmap_group() {
+        let slot_storage = &mut SlotStorage::new();
+
+        let mut remover = BitmapRemover::new();
+
+        let order_id = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+
+        let outer_index = order_id.price_in_ticks.outer_index();
+
+        // 1. Deactivate and check
+        remover.deactivate(slot_storage, &order_id);
+        assert_eq!(outer_index, remover.last_outer_index.unwrap());
+
+        // Expected bitmap group is still blank since it was already off
+        let expected_bitmap_group = BitmapGroup::default();
+        assert_eq!(expected_bitmap_group, remover.bitmap_group);
+
+        let mut read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(BitmapGroup::default(), read_bitmap_group);
+
+        // 2. Write to slot and check
+        remover.write_last_bitmap_group(slot_storage);
+        read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(expected_bitmap_group, read_bitmap_group);
+    }
+
+    #[test]
+    fn deactivate_single_order() {
+        let slot_storage = &mut SlotStorage::new();
+
+        let mut remover = BitmapRemover::new();
+
+        let order_id = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+
+        // First activate the order to set it up
+        let mut bitmap_group = BitmapGroup::default();
+        bitmap_group.inner[0] = 0b00000001;
+        bitmap_group.write_to_slot(slot_storage, &order_id.price_in_ticks.outer_index());
+
+        // 1. Deactivate and check
+        remover.deactivate(slot_storage, &order_id);
+        assert_eq!(
+            order_id.price_in_ticks.outer_index(),
+            remover.last_outer_index.unwrap()
+        );
+
+        let expected_bitmap_group = BitmapGroup::default(); // Bit should now be cleared
+        assert_eq!(expected_bitmap_group, remover.bitmap_group);
+
+        let mut read_bitmap_group =
+            BitmapGroup::new_from_slot(slot_storage, order_id.price_in_ticks.outer_index());
+        assert_eq!(bitmap_group, read_bitmap_group);
+
+        // 2. Write to slot and check
+        remover.write_last_bitmap_group(slot_storage);
+        read_bitmap_group =
+            BitmapGroup::new_from_slot(slot_storage, order_id.price_in_ticks.outer_index());
+        assert_eq!(expected_bitmap_group, read_bitmap_group);
+    }
+
+    #[test]
+    fn deactivate_two_orders_on_same_tick() {
+        let slot_storage = &mut SlotStorage::new();
+
+        let mut remover = BitmapRemover::new();
+
+        let order_id_0 = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+        let order_id_1 = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(1),
+        };
+
+        // First activate the orders to set them up
+        let mut bitmap_group = BitmapGroup::default();
+        bitmap_group.inner[0] = 0b00000011;
+        bitmap_group.write_to_slot(slot_storage, &order_id_0.price_in_ticks.outer_index());
+
+        // 1. Deactivate both and check
+        remover.deactivate(slot_storage, &order_id_0);
+        remover.deactivate(slot_storage, &order_id_1);
+
+        let outer_index = order_id_0.price_in_ticks.outer_index();
+        assert_eq!(outer_index, remover.last_outer_index.unwrap());
+
+        let expected_bitmap_group = BitmapGroup::default(); // Both bits should now be cleared
+        assert_eq!(expected_bitmap_group, remover.bitmap_group);
+
+        let mut read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(bitmap_group, read_bitmap_group);
+
+        // 2. Write to slot and check
+        remover.write_last_bitmap_group(slot_storage);
+        read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(expected_bitmap_group, read_bitmap_group);
+    }
+
+    #[test]
+    fn deactivate_two_orders_on_different_inner_indices_on_same_bitmap_group() {
+        let slot_storage = &mut SlotStorage::new();
+
+        let mut remover = BitmapRemover::new();
+
+        let order_id_0 = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+        let order_id_1 = OrderId {
+            price_in_ticks: Ticks::ONE,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+
+        // First activate the orders to set them up
+        let mut bitmap_group = BitmapGroup::default();
+        bitmap_group.inner[0] = 0b00000001;
+        bitmap_group.inner[1] = 0b00000001;
+        bitmap_group.write_to_slot(slot_storage, &order_id_0.price_in_ticks.outer_index());
+
+        // 1. Deactivate both and check
+        remover.deactivate(slot_storage, &order_id_0);
+        remover.deactivate(slot_storage, &order_id_1);
+
+        let outer_index = order_id_0.price_in_ticks.outer_index();
+        assert_eq!(outer_index, remover.last_outer_index.unwrap());
+
+        let expected_bitmap_group = BitmapGroup::default(); // Both bits should now be cleared
+        assert_eq!(expected_bitmap_group, remover.bitmap_group);
+
+        let mut read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(bitmap_group, read_bitmap_group);
+
+        // 2. Write to slot and check
+        remover.write_last_bitmap_group(slot_storage);
+        read_bitmap_group = BitmapGroup::new_from_slot(slot_storage, outer_index);
+        assert_eq!(expected_bitmap_group, read_bitmap_group);
+    }
+
+    #[test]
+    fn deactivate_two_orders_on_different_bitmap_groups() {
+        let slot_storage = &mut SlotStorage::new();
+
+        let mut remover = BitmapRemover::new();
+
+        let order_id_0 = OrderId {
+            price_in_ticks: Ticks::ZERO,
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+        let order_id_1 = OrderId {
+            price_in_ticks: Ticks::new(32),
+            resting_order_index: RestingOrderIndex::new(0),
+        };
+
+        // First activate the orders to set them up
+        let mut bitmap_group_0 = BitmapGroup::default();
+        bitmap_group_0.inner[0] = 0b00000001;
+        bitmap_group_0.write_to_slot(slot_storage, &order_id_0.price_in_ticks.outer_index());
+
+        let mut bitmap_group_1 = BitmapGroup::default();
+        bitmap_group_1.inner[0] = 0b00000001;
+        bitmap_group_1.write_to_slot(slot_storage, &order_id_1.price_in_ticks.outer_index());
+
+        // 1. Deactivate both and check
+        remover.deactivate(slot_storage, &order_id_0);
+        remover.deactivate(slot_storage, &order_id_1); // this will write the first bitmap group
+
+        let outer_index_0 = order_id_0.price_in_ticks.outer_index();
+        let outer_index_1 = order_id_1.price_in_ticks.outer_index();
+
+        let expected_bitmap_group_0 = BitmapGroup::default();
+        let expected_bitmap_group_1 = BitmapGroup::default();
+
+        // bitmap_group_0 has been written to slot
+        let read_bitmap_group_0 = BitmapGroup::new_from_slot(slot_storage, outer_index_0);
+        assert_eq!(expected_bitmap_group_0, read_bitmap_group_0);
+
+        // bitmap_group_1 and outer_index_1 are still in cache
+        let mut read_bitmap_group_1 = BitmapGroup::new_from_slot(slot_storage, outer_index_1);
+        assert_eq!(bitmap_group_1, read_bitmap_group_1); // Still has the bit on in the cache
+        assert_eq!(outer_index_1, remover.last_outer_index.unwrap());
+        assert_eq!(expected_bitmap_group_1, remover.bitmap_group);
+
+        // 2. Write cache
+        remover.write_last_bitmap_group(slot_storage);
+        read_bitmap_group_1 = BitmapGroup::new_from_slot(slot_storage, outer_index_1);
+        assert_eq!(expected_bitmap_group_1, read_bitmap_group_1);
+    }
+}
