04bfd0271faeed763d50b91698b10b772fa3de74
rollchad <138987133+rollchad@users.noreply.github.com>
2024-09-20 17:33:12 +0200

    test: Correct order id sequence in RemoveMultipleManager
    - Ensure that tests fail if order ids are not correctly sorted.
    - Ensure that jumbled but correctly sorted order ids work.

--- crates/goblin-market/src/program/processor/reduce/remove_multiple_manager.rs
+++ crates/goblin-market/src/program/processor/reduce/remove_multiple_manager.rs
@@ -52,6 +52,11 @@ impl RemoveMultipleManager {
     /// Ensures that successive order ids to remove are sorted in correct order
     ///
     /// Successive IDs must be in ascending order for asks and in descending order for bids
+    ///
+    /// Externally ensure that `remove_order()` is not called if incoming order is not in
+    /// correct order to avoid duplicate removal. This function updates `self.side`
+    /// even though the incoming order is not added to state.
+    ///
     pub(crate) fn check_sorted(&mut self, side: Side, order_id: OrderId) -> GoblinResult<()> {
         self.side = side;
         let last_order_id = self.last_order_id();
@@ -82,5 +87,180 @@ impl RemoveMultipleManager {
     ) {
         self.removers[0].write_prepared_indices(slot_storage, market_state);
         self.removers[1].write_prepared_indices(slot_storage, market_state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod check_sorted {
+        use crate::{
+            quantities::{Ticks, WrapperU64},
+            state::RestingOrderIndex,
+        };
+
+        use super::*;
+
+        #[test]
+        pub fn test_bid_order_sequence_enforced() {
+            let side = Side::Bid;
+
+            let mut manager = RemoveMultipleManager::new(0, 0);
+
+            // First value
+            let order_id_0 = OrderId {
+                price_in_ticks: Ticks::new(4),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(side, order_id_0).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_0);
+
+            // Second- overwrites first
+            let order_id_1 = OrderId {
+                price_in_ticks: Ticks::new(3),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(side, order_id_1).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_1);
+
+            // Third- not in order so fail
+            let order_id_2 = OrderId {
+                price_in_ticks: Ticks::new(5),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            assert!(manager.check_sorted(side, order_id_2).is_err());
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_1);
+
+            // Fourth- higher resting order index
+            let order_id_3 = OrderId {
+                price_in_ticks: Ticks::new(3),
+                resting_order_index: RestingOrderIndex::MAX,
+            };
+            manager.check_sorted(side, order_id_3).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+
+            // Fifth- same tick but lower resting order index so fail
+            let order_id_4 = OrderId {
+                price_in_ticks: Ticks::new(3),
+                resting_order_index: RestingOrderIndex::new(1),
+            };
+            assert!(manager.check_sorted(side, order_id_4).is_err());
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+        }
+
+        #[test]
+        pub fn test_ask_order_sequence_enforced() {
+            let side = Side::Ask;
+            let mut manager = RemoveMultipleManager::new(0, 0);
+
+            // First value
+            let order_id_0 = OrderId {
+                price_in_ticks: Ticks::new(4),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(side, order_id_0).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_0);
+
+            // Second- overwrites first
+            let order_id_1 = OrderId {
+                price_in_ticks: Ticks::new(5),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(side, order_id_1).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_1);
+
+            // Third- not in order so fail
+            let order_id_2 = OrderId {
+                price_in_ticks: Ticks::new(3),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            assert!(manager.check_sorted(side, order_id_2).is_err());
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_1);
+
+            // Fourth- higher resting order index
+            let order_id_3 = OrderId {
+                price_in_ticks: Ticks::new(5),
+                resting_order_index: RestingOrderIndex::MAX,
+            };
+            manager.check_sorted(side, order_id_3).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+
+            // Fifth- same tick but lower resting order index so fail
+            let order_id_4 = OrderId {
+                price_in_ticks: Ticks::new(5),
+                resting_order_index: RestingOrderIndex::new(1),
+            };
+            assert!(manager.check_sorted(side, order_id_4).is_err());
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+        }
+
+        #[test]
+        pub fn test_bids_and_asks_mixed_but_respective_sequence_maintained() {
+            let mut manager = RemoveMultipleManager::new(0, 0);
+
+            // Insert bid, ask, bid, ask in correct sequence,
+            // then add bid, ask in wrong sequence
+
+            // 0. Bid
+            let order_id_0 = OrderId {
+                price_in_ticks: Ticks::new(4),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(Side::Bid, order_id_0).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_0);
+            assert_eq!(manager.side, Side::Bid);
+
+            // 1. Ask
+            let order_id_1 = OrderId {
+                price_in_ticks: Ticks::new(7),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(Side::Ask, order_id_1).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_1);
+            assert_eq!(manager.side, Side::Ask);
+
+            // 2. Bid
+            let order_id_2 = OrderId {
+                price_in_ticks: Ticks::new(3),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(Side::Bid, order_id_2).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_2);
+            assert_eq!(manager.side, Side::Bid);
+
+            // 3. Ask
+            let order_id_3 = OrderId {
+                price_in_ticks: Ticks::new(8),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            manager.check_sorted(Side::Ask, order_id_3).unwrap();
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+            assert_eq!(manager.side, Side::Ask);
+
+            // 4. Out of order bid
+            let order_id_4 = OrderId {
+                price_in_ticks: Ticks::new(5),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+
+            assert!(manager.check_sorted(Side::Bid, order_id_4).is_err());
+
+            // Since side changed, last_order_id will change
+            assert_eq!(manager.side, Side::Bid);
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_2);
+
+            // 5. Out of order ask
+            let order_id_5 = OrderId {
+                price_in_ticks: Ticks::new(6),
+                resting_order_index: RestingOrderIndex::ZERO,
+            };
+            // Last order ID did not change but side changed
+            assert!(manager.check_sorted(Side::Ask, order_id_5).is_err());
+
+            // Since side changed, last_order_id will change
+            assert_eq!(manager.side, Side::Ask);
+            assert_eq!((*manager.last_order_id()).unwrap(), order_id_3);
+        }
     }
 }
