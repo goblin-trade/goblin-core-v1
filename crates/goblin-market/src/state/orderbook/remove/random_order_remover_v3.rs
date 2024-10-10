e67ef33341a64ecf616c206f476eb214dcb7e375
rollchad <138987133+rollchad@users.noreply.github.com>
2024-10-10 13:54:30 +0200

    feat: Integrate ISequentialOrderRemoverV3 with RandomOrderRemoverV3

--- crates/goblin-market/src/state/orderbook/remove/random_order_remover_v3.rs
+++ crates/goblin-market/src/state/orderbook/remove/random_order_remover_v3.rs
@@ -12,6 +12,7 @@ use super::{
     random_outer_index_remover_v3::RandomOuterIndexRemoverV3,
     sequential_order_remover_v2::SequentialOrderRemoverV2,
     sequential_order_remover_v3::ISequentialOrderRemoverV3,
+    sequential_outer_index_remover_v3::ISequentialOuterIndexRemover,
 };
 
 use alloc::vec::Vec;
@@ -30,20 +31,20 @@ pub struct RandomOrderRemoverV3<'a> {
     pub pending_write: bool,
 }
 
-// impl<'a> ISequentialOrderRemoverV3<'a> for RandomOrderRemoverV3<'a> {
-//     fn group_position_remover(&mut self) -> &mut GroupPositionRemoverV2 {
-//         &mut self.group_position_remover
-//     }
+impl<'a> ISequentialOrderRemoverV3<'a> for RandomOrderRemoverV3<'a> {
+    fn group_position_remover(&mut self) -> &mut GroupPositionRemoverV2 {
+        &mut self.group_position_remover
+    }
 
-//     fn outer_index_remover(&mut self) -> &mut SequentialOuterIndexRemoverV3<'a> {
-//         &mut self.outer_index_remover
-//     }
+    fn outer_index_remover(&mut self) -> &mut impl ISequentialOuterIndexRemover<'a> {
+        &mut self.outer_index_remover
+    }
 
-//     fn best_market_price(&mut self) -> &mut Ticks {
-//         &mut self.best_market_price
-//     }
+    fn best_market_price(&mut self) -> &mut Ticks {
+        &mut self.best_market_price
+    }
 
-//     fn pending_write(&mut self) -> &mut bool {
-//         &mut self.pending_write
-//     }
-// }
+    fn pending_write(&mut self) -> &mut bool {
+        &mut self.pending_write
+    }
+}
