e3280aed6ce22b06293fda7f0aca8f9fbf7e3454
rollchad <138987133+rollchad@users.noreply.github.com>
2024-10-12 14:58:42 +0200

    fix: Fix exports warning in removers module

--- crates/goblin-market/src/state/orderbook/remove/mod.rs
+++ crates/goblin-market/src/state/orderbook/remove/mod.rs
@@ -2,6 +2,9 @@ pub mod common;
 pub mod lookup_remover;
 pub mod sequential_remover;
 
-pub use common::*;
-pub use lookup_remover::*;
-pub use sequential_remover::*;
+pub use common::structs::*;
+pub use common::traits::*;
+pub use lookup_remover::structs::*;
+pub use lookup_remover::traits::*;
+pub use sequential_remover::structs::*;
+pub use sequential_remover::traits::*;
