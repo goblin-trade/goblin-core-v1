dbc51dcbcfdf86f88c77247e5451bc17456fdef3
rollchad <138987133+rollchad@users.noreply.github.com>
2025-02-05 13:48:34 +0100

    feat: Return length from handler

--- crates/goblin-core/src/lib.rs
+++ crates/goblin-core/src/lib.rs
@@ -1,13 +1,40 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
-#[no_mangle]
-pub extern "C" fn user_entrypoint(len: usize) -> i32 {
-    0
-}
+extern crate alloc;
+use alloc::vec::Vec;
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+extern "C" {
+    fn read_args(dest: *mut u8);
+    fn write_result(data: *const u8, len: usize);
+}
+
+#[no_mangle]
+pub extern "C" fn user_entrypoint(len: usize) -> i32 {
+    let mut input = Vec::<u8>::with_capacity(len);
+
+    unsafe {
+        // Read the input data
+        read_args(input.as_mut_ptr());
+    }
+
+    // Convert the length to bytes
+    let result = len.to_le_bytes();
+
+    unsafe {
+        // Write the length back as result
+        write_result(result.as_ptr(), result.len());
+    }
+
+    0
 }
