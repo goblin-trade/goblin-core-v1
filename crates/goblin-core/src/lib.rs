f4259221bf15bcaa144ffb8ede376fe53f60dab2
rollchad <138987133+rollchad@users.noreply.github.com>
2025-02-09 14:21:49 +0100

    feat: Benchmark static memory

--- crates/goblin-core/src/lib.rs
+++ crates/goblin-core/src/lib.rs
@@ -23,6 +23,9 @@ pub unsafe extern "C" fn mark_used() {
     panic!();
 }
 
+// Static buffer for input
+static mut INPUT_BUFFER: [u8; 512] = [0; 512];
+
 #[no_mangle]
 pub extern "C" fn user_entrypoint(len: usize) -> i32 {
     if len == 0 {
@@ -30,12 +33,11 @@ pub extern "C" fn user_entrypoint(len: usize) -> i32 {
     }
 
     let (selector, payload) = unsafe {
-        let mut input: [MaybeUninit<u8>; 512] = MaybeUninit::uninit().assume_init();
-        read_args(input.as_mut_ptr() as *mut u8);
+        read_args(INPUT_BUFFER.as_mut_ptr());
 
         (
-            input[0].assume_init(),
-            core::slice::from_raw_parts(input.as_ptr().add(1) as *const u8, len.saturating_sub(1)),
+            INPUT_BUFFER[0],
+            core::slice::from_raw_parts(INPUT_BUFFER.as_ptr().add(1), len.saturating_sub(1)),
         )
     };
 
