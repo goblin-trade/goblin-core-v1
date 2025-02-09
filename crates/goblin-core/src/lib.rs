e275c5be1dca2d0ae9de09052afda5271d8e8ca9
rollchad <138987133+rollchad@users.noreply.github.com>
2025-02-09 13:48:00 +0100

    feat: Use MaybeUninit to avoid zero fill cost

--- crates/goblin-core/src/lib.rs
+++ crates/goblin-core/src/lib.rs
@@ -1,25 +1,21 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+use core::mem::MaybeUninit;
+use handler::handle_deposit_funds;
+use hostio::*;
+
 pub mod handler;
 pub mod hostio;
 pub mod market_params;
 pub mod quantities;
 pub mod selector;
-// extern crate alloc;
-// use alloc::vec::Vec;
-use handler::handle_deposit_funds;
-use hostio::*;
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
-
-// #[cfg(target_arch = "wasm32")]
-// #[global_allocator]
-// static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 #[no_mangle]
 pub unsafe extern "C" fn mark_used() {
@@ -29,24 +25,22 @@ pub unsafe extern "C" fn mark_used() {
 
 #[no_mangle]
 pub extern "C" fn user_entrypoint(len: usize) -> i32 {
-    if len == 0 || len > 90 {
+    if len == 0 {
         return 1;
     }
 
-    // Use fixed-size array on the stack instead of heap allocation
-    let mut input = [0u8; 90];
+    let (selector, payload) = unsafe {
+        let mut input: [MaybeUninit<u8>; 512] = MaybeUninit::uninit().assume_init();
+        read_args(input.as_mut_ptr() as *mut u8);
 
-    unsafe {
-        read_args(input.as_mut_ptr());
-    }
+        (
+            input[0].assume_init(),
+            core::slice::from_raw_parts(input.as_ptr().add(1) as *const u8, len.saturating_sub(1)),
+        )
+    };
 
-    // Extract function selector
-    let selector = input[0];
-    let payload = &input[1..len]; // Only use the actual input length
-
-    // Route to appropriate handler based on selector
-    return match selector {
+    match selector {
         selector::DEPOSIT_FUNDS_SELECTOR => handle_deposit_funds(payload),
         _ => 1,
-    };
+    }
 }
