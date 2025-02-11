aa45d7b97885c3115225dd133fe3354b2e8c7646
rollchad <138987133+rollchad@users.noreply.github.com>
2025-02-11 13:43:28 +0100

    feat: Find keccak hash of market params

--- crates/goblin-core/src/market_params.rs
+++ crates/goblin-core/src/market_params.rs
@@ -1,4 +1,7 @@
-use crate::quantities::{BaseLots, QuoteLots, Ticks};
+use crate::{
+    native_keccak256,
+    quantities::{BaseLots, QuoteLots, Ticks},
+};
 
 #[repr(C, packed)]
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -14,8 +17,24 @@ pub struct MarketParams {
     pub quote_decimals_to_ignore: u8,
 }
 
+impl MarketParams {
+    pub fn keccak256(&self) -> [u8; 32] {
+        let mut output = [0u8; 32];
+        unsafe {
+            native_keccak256(
+                (self as *const Self) as *const u8,
+                core::mem::size_of::<Self>(),
+                output.as_mut_ptr(),
+            );
+        }
+        output
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use tiny_keccak::Hasher;
+
     use super::*;
 
     #[test]
@@ -50,5 +69,34 @@ mod tests {
 
         // Ensure correctness
         assert_eq!(market_params, *deserialized);
+    }
+
+    #[test]
+    fn test_keccak() {
+        let market_params = MarketParams {
+            base_token: [0u8; 20],
+            quote_token: [1u8; 20],
+            base_lot_size: BaseLots(5),
+            quote_lot_size: QuoteLots(2),
+            tick_size: Ticks(1),
+            taker_fee_bps: 2,
+            fee_collector: [3u8; 20],
+            base_decimals_to_ignore: 6,
+            quote_decimals_to_ignore: 6,
+        };
+        let result = market_params.keccak256();
+
+        let bytes = unsafe {
+            std::slice::from_raw_parts(
+                &market_params as *const MarketParams as *const u8,
+                core::mem::size_of::<MarketParams>(),
+            )
+        };
+        let mut hasher = tiny_keccak::Keccak::v256();
+        hasher.update(bytes);
+        let mut expected_hash = [0u8; 32];
+        hasher.finalize(&mut expected_hash);
+
+        assert_eq!(result, expected_hash);
     }
 }
