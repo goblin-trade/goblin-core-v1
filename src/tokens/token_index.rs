6dba5c4a35150d27e3a536bcd4f2982c0be371b9
rollchad <138987133+rollchad@users.noreply.github.com>
2025-07-27 22:28:09 +0200

    doc: Token index to Token lookup comments

--- src/tokens/token_index.rs
+++ src/tokens/token_index.rs
@@ -18,13 +18,16 @@ impl TokenIndex {
     /// * To obtain slot keys for a market
     /// * In the settlement phase for token transfers and slot updates.
     ///
+    /// This is still cheap because we're looking up values for an index within a slice.
+    /// There is no need to store token address in ERC20Delta
+    ///
     /// In the intermediary stages, it is possible that an invalid token index is passed to ERC20Delta.
     /// However this is caught during settlement.
-    ///
-    /// We do not store token address inside ERC20Delta. While this allows us to pass the address
-    /// found in the market key generation phase, it means we need to find the address when converting
-    /// erc20_delta_input_list to erc20_delta_list. We cannot avoid duplicate lookup either way.
     pub fn to_token(&self, custom_erc20_list: &[Address]) -> Result<Token, GoblinError> {
+        if *self == Self::ETH {
+            return Ok(Token::Eth);
+        }
+
         let index = self.0 as usize;
 
         if index < custom_erc20_list.len() {
@@ -33,8 +36,6 @@ impl TokenIndex {
         } else if index > 127 && index < (127 + HARDCODED_TOKENS.len()) {
             let token = HARDCODED_TOKENS[index - 127];
             Ok(Token::ERC20(ERC20Token::Hardcoded(token)))
-        } else if *self == Self::ETH {
-            Ok(Token::Eth)
         } else {
             Err(GoblinError::NoTokenAtIndex)
         }
