e1693811ee8ae2327e1edc1d4e6c56476f9987f5
rollchad <138987133+rollchad@users.noreply.github.com>
2025-11-28 21:09:24 +0200

    refactor: Reintroduce global update struct

--- src/settlement/global_delta/global_sender_delta/mod.rs
+++ src/settlement/global_delta/global_sender_delta/mod.rs
@@ -1,7 +1,11 @@
+pub mod common_delta;
 pub mod eth_delta;
 pub mod global_sender_delta;
+pub mod global_update;
 pub mod token_deltas;
 
+pub use common_delta::*;
 pub use eth_delta::*;
 pub use global_sender_delta::*;
+pub use global_update::*;
 pub use token_deltas::*;
