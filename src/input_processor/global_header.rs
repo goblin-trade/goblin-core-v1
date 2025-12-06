32c926fec995702a2d86411aed943e8f327674f7
rollchad <138987133+rollchad@users.noreply.github.com>
2025-12-06 16:20:45 +0200

    feat: Read market counts in global header

--- src/input_processor/global_header.rs
+++ src/input_processor/global_header.rs
@@ -1,7 +1,7 @@
 use crate::{
     goblin_error::GoblinError,
     hostio::{self},
-    input_processor::{ArgsBuffer, ArgsDecoder, HeaderFlags},
+    input_processor::{ArgsBuffer, ArgsDecoder, Decodable, HeaderFlags, MarketCounts},
     quantities::{QuantityOps, UnsidedAtoms},
     tokens::CustomToken,
     types::{Address, NATIVE_TOKEN_DECIMALS},
@@ -11,6 +11,9 @@ use crate::{
 pub struct GlobalHeader<'a> {
     /// Flags and counts. Tells whether optional values should be read.
     pub flags: HeaderFlags,
+
+    /// Number of markets to decode, namespaced by type
+    pub market_counts: MarketCounts,
 
     /// ETH atoms deposited via msg_value
     pub msg_value: UnsidedAtoms,
@@ -30,33 +33,36 @@ impl<'a> GlobalHeader<'a> {
     ///
     /// The API is similar to Decodable trait includes the 'a lifetime that Decodable lacks
     pub fn new(args: &'a ArgsBuffer, offset: &mut usize, len: usize) -> Result<Self, GoblinError> {
-        let header = HeaderFlags::init(args, len)?;
+        let flags = HeaderFlags::init(args, len)?;
         *offset = HeaderFlags::HEADER_BYTE_SIZE;
 
-        let msg_value = if header.track_msg_value {
+        let market_counts = MarketCounts::decode(args, offset, len)?;
+
+        let msg_value = if flags.track_msg_value {
             let msg_value_raw = hostio::msg_value();
             UnsidedAtoms::from_raw_atoms(msg_value_raw.as_ref(), NATIVE_TOKEN_DECIMALS)?
         } else {
             UnsidedAtoms::ZERO
         };
 
-        let eth_out_due = if header.withdraw_eth {
+        let eth_out_due = if flags.withdraw_eth {
             *args.decode_ref_unchecked::<UnsidedAtoms>(offset)
         } else {
             UnsidedAtoms::ZERO
         };
 
-        let recipient = if header.recipient_provided {
+        let recipient = if flags.recipient_provided {
             Some(args.decode_ref_unchecked::<Address>(offset))
         } else {
             None
         };
 
         let custom_erc20_list =
-            args.decode_slice_unchecked::<CustomToken>(offset, header.custom_erc20_count);
+            args.decode_slice_unchecked::<CustomToken>(offset, flags.custom_erc20_count);
 
         Ok(GlobalHeader {
-            flags: header,
+            flags,
+            market_counts,
             recipient,
             msg_value,
             eth_out_due,
--- src/input_processor/market_counts.rs
+++ src/input_processor/market_counts.rs
@@ -1,10 +1,17 @@
 use crate::{
-    input_processor::Decodable,
+    goblin_error::GoblinError,
+    input_processor::{ArgsDecoder, Decodable},
     markets::{MarketVariantMap, PairShapeMap},
+    require,
 };
 
-pub type PairShapeMarkets = PairShapeMap<u8>;
+const BYTE_COUNT: usize = 3;
+
+/// The number of markets of each type
 pub type MarketCounts = MarketVariantMap<PairShapeMarkets>;
+
+/// Market counts per pair shape
+pub type PairShapeMarkets = PairShapeMap<u8>;
 
 impl Decodable<Self> for MarketCounts {
     fn decode(
@@ -12,6 +19,27 @@ impl Decodable<Self> for MarketCounts {
         offset: &mut usize,
         len: usize,
     ) -> Result<Self, crate::goblin_error::GoblinError> {
-        todo!()
+        require!(len >= *offset + BYTE_COUNT, GoblinError::InvalidPayload);
+
+        let byte_0 = args.decode_unchecked::<u8>(*offset);
+        let byte_1 = args.decode_unchecked::<u8>(*offset);
+        let byte_2 = args.decode_unchecked::<u8>(*offset);
+
+        let market_counts = MarketVariantMap {
+            hardcoded: PairShapeMap {
+                eth_erc20: byte_0 & 0b0000_1111,
+                erc20_eth: byte_0 >> 4,
+                erc20_erc20: byte_1 & 0b0000_1111,
+            },
+            dynamic: PairShapeMap {
+                eth_erc20: byte_1 >> 4,
+                erc20_eth: byte_2 & 0b0000_1111,
+                erc20_erc20: byte_2 >> 4,
+            },
+        };
+
+        *offset += BYTE_COUNT;
+
+        Ok(market_counts)
     }
 }
