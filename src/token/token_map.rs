8a659ed778fa3d7649532fbdc655270f55668384
rollchad <138987133+rollchad@users.noreply.github.com>
2025-12-09 10:18:57 +0200

    refactor: Use const N to reduce to single marker

--- src/token/token_map.rs
+++ src/token/token_map.rs
@@ -1,6 +1,6 @@
 use core::marker::PhantomData;
 
-use crate::token::{CustomToken, HardcodedToken, TokenMarker, ERC20, ETH};
+use crate::token::{ERC20, ETH};
 
 pub struct GenericMap<T0, T1, K>(T0, T1, PhantomData<K>);
 
@@ -19,10 +19,9 @@ pub trait GenericMapAccessor<T0, T1, K> {
 // ETH and ERC20 implement trait TokenMarker
 type TokenMap<T0, T1> = GenericMap<T0, T1, (ETH, ERC20)>;
 
-pub struct Marker0<K>(PhantomData<K>);
-pub struct Marker1<K>(PhantomData<K>);
+pub struct Marker<const N: usize, K>(PhantomData<K>);
 
-impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker0<K> {
+impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker<0, K> {
     type Result = T0;
 
     fn get_leg(map: &GenericMap<T0, T1, K>) -> &Self::Result {
@@ -30,7 +29,7 @@ impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker0<K> {
     }
 }
 
-impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker1<K> {
+impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker<1, K> {
     type Result = T1;
 
     fn get_leg(map: &GenericMap<T0, T1, K>) -> &Self::Result {
@@ -41,15 +40,6 @@ impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker1<K> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // fn get_for_leg<T0, T1, K>(
-    //     token_map: &TokenMap<T0, T1>,
-    // ) -> &<M as GenericMapAccessor<T0, T1, K>>::Result
-    // where
-    //     M: GenericMapAccessor<T0, T1, K>,
-    // {
-    //     M::get_leg(token_map)
-    // }
 
     fn process_leg<T0, T1, M>(map: &TokenMap<T0, T1>) -> &M::Result
     where
@@ -69,11 +59,11 @@ mod tests {
     fn test_read_from_token_map() {
         let token_map = TokenMap::<u8, u16>::new(0, 1);
 
-        let eth_amount = Marker0::<(ETH, ERC20)>::get_leg(&token_map);
+        let eth_amount = Marker::<0, (ETH, ERC20)>::get_leg(&token_map);
 
-        let eth_amount_v2 = process_leg::<u8, u16, Marker0<(ETH, ERC20)>>(&token_map);
+        let eth_amount_v2 = process_leg::<u8, u16, Marker<0, (ETH, ERC20)>>(&token_map);
 
-        let eth_amount_v3 = process_leg_typed::<Marker0<(ETH, ERC20)>>(&token_map);
+        let eth_amount_v3 = process_leg_typed::<Marker<0, (ETH, ERC20)>>(&token_map);
 
         // Alt design where (ETH, ERC20) impl MarkerPair
         // (ETH, ERC20)::Markers.0::get_leg()
