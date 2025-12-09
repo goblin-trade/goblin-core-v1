46ab87a7f8d7132cdf1546520aecd4dc6c3d0ef6
rollchad <138987133+rollchad@users.noreply.github.com>
2025-12-09 10:05:01 +0200

    feat: Use generic to label markers

--- src/token/token_map.rs
+++ src/token/token_map.rs
@@ -2,107 +2,80 @@ use core::marker::PhantomData;
 
 use crate::token::{CustomToken, HardcodedToken, TokenMarker, ERC20, ETH};
 
-pub struct GenericMap<T0, T1, M0, M1>(T0, T1, PhantomData<(M0, M1)>);
+pub struct GenericMap<T0, T1, K>(T0, T1, PhantomData<K>);
 
-impl<T0, T1, M0, M1> GenericMap<T0, T1, M0, M1> {
+impl<T0, T1, K> GenericMap<T0, T1, K> {
     pub fn new(t0: T0, t1: T1) -> Self {
         Self(t0, t1, PhantomData)
     }
 }
 
-pub trait GenericMapAccessor<T0, T1, M0, M1> {
+pub trait GenericMapAccessor<T0, T1, K> {
     type Result;
 
-    fn get_leg(map: &GenericMap<T0, T1, M0, M1>) -> &Self::Result;
+    fn get_leg(map: &GenericMap<T0, T1, K>) -> &Self::Result;
 }
 
 // ETH and ERC20 implement trait TokenMarker
-type TokenMap<T0, T1> = GenericMap<T0, T1, ETH, ERC20>;
+type TokenMap<T0, T1> = GenericMap<T0, T1, (ETH, ERC20)>;
 
-// We have more such pairs like (HardcodedToken and CustomToken)
-// Can we have an abstraction to avoid implementing accessor trait for each pair?
+pub struct Marker0<K>(PhantomData<K>);
+pub struct Marker1<K>(PhantomData<K>);
 
-// // Getters for (ETH, ERC20)
-// impl<T0, T1> GenericMapAccessor<T0, T1, ETH, ERC20> for ETH {
-//     type Result = T0;
-
-//     fn get_leg(map: &TokenMap<T0, T1>) -> &Self::Result {
-//         &map.0
-//     }
-// }
-
-// impl<T0, T1> GenericMapAccessor<T0, T1, ETH, ERC20> for ERC20 {
-//     type Result = T1;
-
-//     fn get_leg(map: &TokenMap<T0, T1>) -> &Self::Result {
-//         &map.1
-//     }
-// }
-
-pub struct Marker0;
-pub struct Marker1;
-
-pub trait MapMarker {
-    type Marker;
-}
-
-impl MapMarker for ETH {
-    type Marker = Marker0;
-}
-
-impl MapMarker for ERC20 {
-    type Marker = Marker1;
-}
-
-impl<T0, T1, M0, M1> GenericMapAccessor<T0, T1, M0, M1> for Marker0 {
+impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker0<K> {
     type Result = T0;
 
-    fn get_leg(map: &GenericMap<T0, T1, M0, M1>) -> &Self::Result {
+    fn get_leg(map: &GenericMap<T0, T1, K>) -> &Self::Result {
         &map.0
     }
 }
 
-impl<T0, T1, M0, M1> GenericMapAccessor<T0, T1, M0, M1> for Marker1 {
+impl<T0, T1, K> GenericMapAccessor<T0, T1, K> for Marker1<K> {
     type Result = T1;
 
-    fn get_leg(map: &GenericMap<T0, T1, M0, M1>) -> &Self::Result {
+    fn get_leg(map: &GenericMap<T0, T1, K>) -> &Self::Result {
         &map.1
     }
 }
-// problem- trait overlap
-// Even if we set trait bounds M0: First and M1: Second, it is possible for
-// a marker to implement both traits
-// This problem doesn't arise when we use concrete marker structs.
-
-// Think from the top
-// In PairShape::update<ETH, ERC20>() we first explicitly set generic to ETH
-// The top level value is known, rest of the inner functions become generic.
-//
-// For a struct of keys <ETH, ERC20>, we could have separate traits to operate on first
-// and second elements because the element is always known at the top level.
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn get_for_leg<T0, T1, M>(
-        token_map: &TokenMap<T0, T1>,
-    ) -> &<M as GenericMapAccessor<T0, T1, ETH, ERC20>>::Result
+    // fn get_for_leg<T0, T1, K>(
+    //     token_map: &TokenMap<T0, T1>,
+    // ) -> &<M as GenericMapAccessor<T0, T1, K>>::Result
+    // where
+    //     M: GenericMapAccessor<T0, T1, K>,
+    // {
+    //     M::get_leg(token_map)
+    // }
+
+    fn process_leg<T0, T1, M>(map: &TokenMap<T0, T1>) -> &M::Result
     where
-        M: GenericMapAccessor<T0, T1, ETH, ERC20>,
+        M: GenericMapAccessor<T0, T1, (ETH, ERC20)>,
     {
-        M::get_leg(token_map)
+        M::get_leg(map)
+    }
+
+    fn process_leg_typed<M>(map: &TokenMap<u8, u16>) -> &M::Result
+    where
+        M: GenericMapAccessor<u8, u16, (ETH, ERC20)>,
+    {
+        M::get_leg(map)
     }
 
     #[test]
     fn test_read_from_token_map() {
         let token_map = TokenMap::<u8, u16>::new(0, 1);
 
-        let gg = <ETH as MapMarker>::Marker::get_leg(&token_map);
-        // let eth_amount = ETH::get_leg(&token_map);
+        let eth_amount = Marker0::<(ETH, ERC20)>::get_leg(&token_map);
 
-        // let eth_amount_v2 = get_for_leg::<u8, u16, ETH>(&token_map);
+        let eth_amount_v2 = process_leg::<u8, u16, Marker0<(ETH, ERC20)>>(&token_map);
 
-        // let erc_amount_v2 = get_for_leg::<u8, u16, ERC20>(&token_map);
+        let eth_amount_v3 = process_leg_typed::<Marker0<(ETH, ERC20)>>(&token_map);
+
+        // Alt design where (ETH, ERC20) impl MarkerPair
+        // (ETH, ERC20)::Markers.0::get_leg()
     }
 }
