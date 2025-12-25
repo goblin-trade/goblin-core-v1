9dc64f85f060de98aeeb4ec4db5f80581b34b731
rollchad <138987133+rollchad@users.noreply.github.com>
2025-12-25 13:28:03 +0200

    refactor: Move MarketAndKey to outer module

--- src/market/market_variant/mod.rs
+++ src/market/market_variant/mod.rs
@@ -1,9 +1,7 @@
 pub mod dynamic;
 pub mod hardcoded;
-pub mod market_and_key;
 pub mod market_variant;
 
 pub use dynamic::*;
 pub use hardcoded::*;
-pub use market_and_key::*;
 pub use market_variant::*;
--- src/market/mod.rs
+++ src/market/mod.rs
@@ -1,9 +1,11 @@
 pub mod common_market;
+pub mod market_and_key;
 pub mod market_header;
 pub mod market_variant;
 pub mod pair_shape;
 
 pub use common_market::*;
+pub use market_and_key::*;
 pub use market_header::*;
 pub use market_variant::*;
 pub use pair_shape::*;
