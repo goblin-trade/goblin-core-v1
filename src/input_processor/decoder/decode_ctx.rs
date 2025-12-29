29c753d97a6110e416b06c2c8f7a96baf3a8ea99
rollchad <138987133+rollchad@users.noreply.github.com>
2025-12-29 12:40:14 +0200

    feat: Port decoding functions to DecodeCtx

--- src/input_processor/decoder/decode_ctx.rs
+++ src/input_processor/decoder/decode_ctx.rs
@@ -1,4 +1,7 @@
-use crate::input_processor::ArgsBuffer;
+use crate::{
+    goblin_error::GoblinError,
+    input_processor::{ArgsBuffer, DecodePrimitive},
+};
 use core::cell::Cell;
 
 pub struct DecodeCtx<'a> {
@@ -12,5 +15,50 @@ impl<'a> DecodeCtx<'a> {
             args: &args_buffer[..len],
             offset: Cell::<usize>::default(),
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.args.len()
+    }
+
+    // Zero copy decode
+    fn decode_ref_unchecked<T>(&self) -> &T {
+        let start = self.offset.get();
+        let end = start + core::mem::size_of::<T>();
+        self.offset.set(end);
+
+        unsafe { &*(self.args[start..end].as_ptr() as *const T) }
+    }
+
+    fn decode_slice_unchecked<T>(&self, slice_len: usize) -> &[T] {
+        let start = self.offset.get();
+        let end = start + slice_len * core::mem::size_of::<T>();
+        self.offset.set(end);
+
+        unsafe {
+            core::slice::from_raw_parts(self.args[start..end].as_ptr() as *const T, slice_len)
+        }
+    }
+
+    fn decode<T: DecodePrimitive>(&self) -> Result<T, GoblinError> {
+        let offset = self.offset.get();
+        let size = core::mem::size_of::<T>();
+        crate::require!(
+            self.len() >= offset + size,
+            crate::goblin_error::GoblinError::InvalidPayload
+        );
+
+        let value = Self::decode_unchecked::<T>(self);
+        self.offset.set(offset + size);
+
+        Ok(value)
+    }
+
+    // TODO improvements
+    //
+    // - This is just a proxy to DecodePrimitive. Can we replace with common Decodable trait?
+    // - Does not advance offset unlike the zero copy versions
+    fn decode_unchecked<T: DecodePrimitive>(&self) -> T {
+        T::from_le_bytes_at(self.args, self.args.len())
     }
 }
