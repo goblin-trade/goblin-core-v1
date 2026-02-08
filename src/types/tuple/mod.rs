7aceba410ed456f33e957a08b2bc6533b8d55a05
rollchad <138987133+rollchad@users.noreply.github.com>
2026-02-08 13:05:48 +0200

    refactor: Remove unused ERC20Pair tuple
    Replaced by triple

--- src/types/tuple/mod.rs
+++ src/types/tuple/mod.rs
@@ -1,11 +1,9 @@
-pub mod erc20_pair;
 pub mod impl_tuple_reader;
 pub mod market_variant_pair;
 pub mod pair;
 pub mod tuple;
 pub mod tuple_v2;
 
-pub use erc20_pair::*;
 pub use market_variant_pair::*;
 pub use pair::*;
 pub use tuple::*;
