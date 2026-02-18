02251329a4983227876d64ad17f478a70d4d151b
rollchad <138987133+rollchad@users.noreply.github.com>
2026-02-18 13:10:41 +0200

    feat: Coordinate trait and iterator for Column

--- src/matching/bitmap/inner_bitmap/column.rs
+++ src/matching/bitmap/inner_bitmap/column.rs
@@ -1,4 +1,7 @@
-use crate::{axis::leg::leg_matcher::LegMatcher, matching::bitmap::inner_pos::InnerPos};
+use crate::{
+    axis::leg::leg_matcher::LegMatcher,
+    matching::bitmap::{inner_pos::InnerPos, Coordinate},
+};
 
 pub struct Column {
     pub inner: u8,
@@ -7,6 +10,16 @@ pub struct Column {
 impl Column {
     pub const fn new(inner: u8) -> Self {
         Self { inner }
+    }
+}
+
+impl Coordinate for Column {
+    const MIN: Self = Self::new(0);
+
+    const MAX: Self = Self::new(7);
+
+    fn iter(self) -> impl Iterator<Item = Self> {
+        (Self::MIN.inner..=Self::MAX.inner).map(Self::new)
     }
 }
 
