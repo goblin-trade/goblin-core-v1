005e11874e23e558c4d270739f126f1510b15896
rollchad <138987133+rollchad@users.noreply.github.com>
2026-02-26 11:38:37 +0200

    wip: MarketItem proxy struct for common API

---
+++ src/matching/active_iterator/outer_bitmap/market_item.rs
@@ -0,0 +1,74 @@
+use core::marker::PhantomData;
+
+use crate::{
+    axis::{
+        leg::leg_matcher::LegMatcher, market::market_marker::MarketMarker,
+        token::token_marker::TokenMarker,
+    },
+    matching::{
+        active_iterator::outer_bitmap::outer_bitmap_item::OuterBitmapItem,
+        bitmap::{outer_bitmap_index::OuterBitmapIndex, range::Range, Coordinate},
+    },
+    state::{
+        outer_bitmap::{outer_bitmap_state::OuterBitmapState, preimage::OuterBitmapPreimage},
+        MarketPreimage, Preimage, SlotKey,
+    },
+};
+
+/// Proxy struct for market key with helper function to get OuterBitmapItem
+pub struct MarketItem<'a, M, B, Q, In>
+where
+    M: MarketMarker,
+    B: TokenMarker,
+    Q: TokenMarker,
+    In: LegMatcher,
+{
+    /// Market key
+    pub market_key: &'a SlotKey<MarketPreimage<M, B, Q>>,
+
+    _marker: PhantomData<In>,
+}
+
+impl<'a, M, B, Q, In> MarketItem<'a, M, B, Q, In>
+where
+    M: MarketMarker,
+    B: TokenMarker,
+    Q: TokenMarker,
+    In: LegMatcher,
+{
+    pub fn new(market_key: &'a SlotKey<MarketPreimage<M, B, Q>>) -> Self {
+        Self {
+            market_key,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn get_outer_bitmap_item(
+        &self,
+        range: Range<OuterBitmapIndex<In>>,
+    ) -> Option<OuterBitmapItem<M, B, Q, In>> {
+        if range.limit.closer_to_centre(range.start) {
+            return None;
+        }
+        let limit_reached = range.on_limit();
+
+        let preimage = OuterBitmapPreimage {
+            market_key: *self.market_key,
+            outer_bitmap_index: range.start,
+        };
+        let outer_bitmap_key = preimage.hash();
+        let outer_bitmap = outer_bitmap_key.load();
+        let outer_bitmap_state = OuterBitmapState::from(outer_bitmap);
+
+        if let OuterBitmapState::Active(active_outer_bitmap) = outer_bitmap_state {
+            return Some(OuterBitmapItem {
+                outer_bitmap_index: range.start,
+                outer_bitmap_key,
+                active_outer_bitmap,
+                on_limit: limit_reached,
+            });
+        }
+
+        None
+    }
+}
--- src/matching/active_iterator/outer_bitmap/mod.rs
+++ src/matching/active_iterator/outer_bitmap/mod.rs
@@ -2,4 +2,5 @@ pub mod active_outer_bitmap_iterator;
 pub use active_outer_bitmap_iterator::*;
 
 mod impl_iterator;
+pub mod market_item;
 pub mod outer_bitmap_item;
