0edd9e6f41adb293268464156e8aef4880f96e40
rollchad <138987133+rollchad@users.noreply.github.com>
2026-03-16 15:06:34 +0200

    feat: GenericIterator::new()

--- src/matching/generic_iterator/generic_iterator.rs
+++ src/matching/generic_iterator/generic_iterator.rs
@@ -1,10 +1,14 @@
 use core::marker::PhantomData;
+use core::ops::RangeInclusive;
+
+use crate::goblin_error::GoblinError;
 
 pub struct GenericIterator<I, L, N>
 where
-    I: Iterator,
+    I: Iterator + Clone + Copy,
     I::Item: InnerItem<L::Item, N>,
-    L: Iterator,
+    L: Iterator + From<RangeInclusive<L::Item>>,
+    L::Item: Clone + Copy,
 {
     pub inner_iterator: I,
     pub inner_item: I::Item,
@@ -17,11 +21,38 @@ pub trait InnerItem<P, N> {
     fn next_item(&self, pos: P) -> Option<N>;
 }
 
+impl<I, L, N> GenericIterator<I, L, N>
+where
+    I: Iterator + Clone + Copy,
+    I::Item: InnerItem<L::Item, N>,
+    L: Iterator + From<RangeInclusive<L::Item>>,
+    L::Item: Clone + Copy,
+{
+    pub fn new(
+        inner_iterator: I,
+        linear_range: RangeInclusive<L::Item>,
+    ) -> Result<Self, GoblinError> {
+        let inner_item = inner_iterator
+            .clone()
+            .next()
+            .ok_or(GoblinError::IteratorOutOfBounds)?;
+
+        Ok(Self {
+            inner_iterator,
+            inner_item,
+            limit: *linear_range.end(),
+            linear_iterator: linear_range.into(),
+            _marker: PhantomData,
+        })
+    }
+}
+
 impl<I, L, N> Iterator for GenericIterator<I, L, N>
 where
-    I: Iterator,
+    I: Iterator + Clone + Copy,
     I::Item: InnerItem<L::Item, N>,
-    L: Iterator,
+    L: Iterator + From<RangeInclusive<L::Item>>,
+    L::Item: Clone + Copy,
 {
     type Item = N;
 
