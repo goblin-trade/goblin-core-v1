5e6c149a4e85f9486f57c26f27b1e24c03fea446
rollchad <138987133+rollchad@users.noreply.github.com>
2026-04-10 09:35:20 +0200

    feat: Turn BitmapIndex into an alias of DerivedPosition

--- src/state/slot/matrix/bitmap_v2/bitmap_index_v2.rs
+++ src/state/slot/matrix/bitmap_v2/bitmap_index_v2.rs
@@ -1,26 +1,38 @@
-use crate::{
-    quantities::{InnerPosV2, OuterPosV2},
-    state::bitmap_v2::ordered_index::OrderedIndex,
-};
+use crate::{quantities::DerivedPosition, state::bitmap_v2::ordered_index::OrderedIndex};
 
-pub trait BitmapIndexV2: OrderedIndex {
-    fn inner(&self) -> u8;
-    fn byte_index(&self) -> usize {
-        self.inner() as usize / 8
+pub type BitmapIndexV2<const BIT_OFFSET: usize, const BIT_COUNT: usize> =
+    DerivedPosition<u8, BIT_OFFSET, BIT_COUNT>;
+
+impl<const BIT_OFFSET: usize, const BIT_COUNT: usize> BitmapIndexV2<BIT_OFFSET, BIT_COUNT>
+where
+    Self: OrderedIndex,
+{
+    pub fn byte_index(&self) -> usize {
+        self.inner as usize / 8
     }
-    fn bit_index(&self) -> usize {
-        self.inner() as usize % 8
+
+    pub fn bit_index(&self) -> usize {
+        self.inner as usize % 8
     }
 }
+// pub trait BitmapIndexV2: OrderedIndex {
+//     fn inner(&self) -> u8;
+//     fn byte_index(&self) -> usize {
+//         self.inner() as usize / 8
+//     }
+//     fn bit_index(&self) -> usize {
+//         self.inner() as usize % 8
+//     }
+// }
 
-impl BitmapIndexV2 for OuterPosV2 {
-    fn inner(&self) -> u8 {
-        self.inner
-    }
-}
+// impl BitmapIndexV2 for OuterPosV2 {
+//     fn inner(&self) -> u8 {
+//         self.inner
+//     }
+// }
 
-impl BitmapIndexV2 for InnerPosV2 {
-    fn inner(&self) -> u8 {
-        self.inner
-    }
-}
+// impl BitmapIndexV2 for InnerPosV2 {
+//     fn inner(&self) -> u8 {
+//         self.inner
+//     }
+// }
--- src/state/slot/matrix/bitmap_v2/bitmap_v2.rs
+++ src/state/slot/matrix/bitmap_v2/bitmap_v2.rs
@@ -1,38 +1,65 @@
 use crate::axis::leg::leg_matcher::LegMatcher;
 use crate::state::bitmap_v2::bitmap_index_v2::BitmapIndexV2;
 use crate::state::bitmap_v2::ordered_index::OrderedIndex;
-use core::marker::PhantomData;
 use core::ops::RangeInclusive;
 
-// Alt design to get rid of BitmapIndex trait
-// Instead of I: BitmapIndexV2, use Derived position directly with K = 8 and rest as generics
-//
-// inside the impl block, add a restriction bound OrderedIndex
-pub struct BitmapV2<I>
-where
-    I: BitmapIndexV2,
-    I::Prev: OrderedIndex,
-{
+pub struct BitmapV2<const BIT_OFFSET: usize, const BIT_COUNT: usize> {
     pub inner: [u8; 32],
-    _marker: PhantomData<I>,
 }
 
-impl<I> BitmapV2<I>
+impl<const BIT_OFFSET: usize, const BIT_COUNT: usize> BitmapV2<BIT_OFFSET, BIT_COUNT>
 where
-    I: BitmapIndexV2,
-    I::Prev: OrderedIndex,
+    BitmapIndexV2<BIT_OFFSET, BIT_COUNT>: OrderedIndex,
 {
-    pub fn index_active(&self, index: I) -> bool {
+    pub fn index_active(&self, index: BitmapIndexV2<BIT_OFFSET, BIT_COUNT>) -> bool {
         let byte = self.inner[index.byte_index()];
         let mask = 1 << index.bit_index();
 
         (byte & mask) != 0
     }
 
-    pub fn active_iterator<In>(self, clamped_range: RangeInclusive<I>) -> impl Iterator<Item = I>
+    pub fn active_iterator<In>(
+        self,
+        clamped_range: RangeInclusive<BitmapIndexV2<BIT_OFFSET, BIT_COUNT>>,
+    ) -> impl Iterator<Item = BitmapIndexV2<BIT_OFFSET, BIT_COUNT>>
     where
         In: LegMatcher,
     {
-        I::get_iter::<In>(clamped_range).filter(move |index| self.index_active(*index))
+        BitmapIndexV2::<BIT_OFFSET, BIT_COUNT>::get_iter::<In>(clamped_range)
+            .filter(move |index| self.index_active(*index))
+        // I::get_iter::<In>(clamped_range).filter(move |index| self.index_active(*index))
     }
 }
+
+// Alt design to get rid of BitmapIndex trait
+// Instead of I: BitmapIndexV2, use Derived position directly with K = 8 and rest as generics
+//
+// inside the impl block, add a restriction bound OrderedIndex
+// pub struct BitmapV2<I>
+// where
+//     I: BitmapIndexV2,
+//     I::Prev: OrderedIndex,
+// {
+//     pub inner: [u8; 32],
+//     _marker: PhantomData<I>,
+// }
+
+// impl<I> BitmapV2<I>
+// where
+//     I: BitmapIndexV2,
+//     I::Prev: OrderedIndex,
+// {
+//     pub fn index_active(&self, index: I) -> bool {
+//         let byte = self.inner[index.byte_index()];
+//         let mask = 1 << index.bit_index();
+
+//         (byte & mask) != 0
+//     }
+
+//     pub fn active_iterator<In>(self, clamped_range: RangeInclusive<I>) -> impl Iterator<Item = I>
+//     where
+//         In: LegMatcher,
+//     {
+//         I::get_iter::<In>(clamped_range).filter(move |index| self.index_active(*index))
+//     }
+// }
