cf37d08ea72825aead93f9625d43b160e2d08d6f
rollchad <138987133+rollchad@users.noreply.github.com>
2026-04-10 09:40:30 +0200

    wip: Update BitmapPreimage definition

--- src/state/slot/matrix/bitmap_v2/preimage.rs
+++ src/state/slot/matrix/bitmap_v2/preimage.rs
@@ -1,17 +1,14 @@
 use crate::{
     axis::{market::market_marker::MarketMarker, token::token_marker::TokenMarker},
     state::{
-        bitmap_v2::{
-            bitmap_index_v2::BitmapIndexV2, ordered_index::OrderedIndex, outer_index::OuterIndex,
-            BitmapV2,
-        },
+        bitmap_v2::{ordered_index::OrderedIndex, outer_index::OuterIndex, BitmapV2},
         MarketPreimage, Preimage, SlotKey,
     },
 };
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct BitmapPreimageV2<M, B, Q, I>
+pub struct BitmapPreimageV2<M, B, Q, I, const BIT_OFFSET: usize, const BIT_COUNT: usize>
 where
     M: MarketMarker,
     B: TokenMarker,
@@ -23,16 +20,17 @@ where
     pub outer_index: OuterIndex<I>,
 }
 
-impl<M, B, Q, I> Preimage for BitmapPreimageV2<M, B, Q, I>
+impl<M, B, Q, I, const BIT_OFFSET: usize, const BIT_COUNT: usize> Preimage
+    for BitmapPreimageV2<M, B, Q, I, BIT_OFFSET, BIT_COUNT>
 where
     M: MarketMarker,
     B: TokenMarker,
     Q: TokenMarker,
-    I: BitmapIndexV2,
+    I: OrderedIndex,
     I::Prev: OrderedIndex,
 {
     // Discriminator 5 for both bitmaps
     // TODO update RestingOrderBitmap with discriminator 6
     const SLOT_DISCRIMINATOR: u8 = 5;
-    type SlotState = BitmapV2<I>;
+    type SlotState = BitmapV2<BIT_OFFSET, BIT_COUNT>;
 }
