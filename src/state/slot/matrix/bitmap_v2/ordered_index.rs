cf8adc13c99044c0d9889de8eaf35fae17dd41c4
rollchad <138987133+rollchad@users.noreply.github.com>
2026-04-15 09:50:55 +0200

    feat: Move iterators on OrderedIndex trait

--- src/state/slot/matrix/bitmap_v2/ordered_index.rs
+++ src/state/slot/matrix/bitmap_v2/ordered_index.rs
@@ -3,10 +3,12 @@ use crate::{
         leg::leg_matcher::LegMatcher, market::market_marker::MarketMarker,
         token::token_marker::TokenMarker,
     },
-    quantities::{InnerPosV2, OuterBitmapIndexV2, OuterPosV2},
-    state::{bitmap_v2::outer_index::OuterIndex, MarketPreimage, SlotKey},
+    quantities::{InnerPosV2, OuterBitmapIndexV2, OuterPosV2, INNER_POS_V2, OUTER_POS_V2},
+    state::{
+        bitmap_v2::{outer_index::OuterIndex, preimage::BitmapPreimageV2},
+        MarketPreimage, Preimage, SlotKey,
+    },
 };
-use core::default;
 use core::ops::RangeInclusive;
 
 pub trait OrderedIndex: Clone + Copy + PartialEq + Default {
@@ -89,6 +91,45 @@ impl OrderedIndex for OuterPosV2 {
     {
         In::outer_pos_iter(range)
     }
+
+    fn parent_iterator<M, B, Q, In>(
+        market_key: SlotKey<MarketPreimage<M, B, Q>>,
+        range: RangeInclusive<(OuterIndex<Self>, Self)>,
+    ) -> impl Iterator<Item = (OuterIndex<Self>, impl Iterator<Item = Self>)>
+    where
+        M: MarketMarker,
+        B: TokenMarker,
+        Q: TokenMarker,
+        In: LegMatcher,
+    {
+        let (start, end) = range.clone().into_inner();
+        let outer_range = start.0 .1..=end.0 .1;
+
+        OuterBitmapIndexV2::linear_iterator::<In>(outer_range).filter_map(
+            move |outer_bitmap_index| {
+                let outer_index = ((), outer_bitmap_index);
+
+                let preimage = BitmapPreimageV2::<M, B, Q, OUTER_POS_V2> {
+                    market_key,
+                    outer_index,
+                };
+                let hash = preimage.hash();
+
+                let outer_bitmap = hash.load();
+
+                if !outer_bitmap.is_active() {
+                    return None;
+                }
+
+                let outer_pos_range = Self::clamped_range::<In>(&range, outer_index);
+
+                let outer_pos_iterator = OuterPosV2::linear_iterator::<In>(outer_pos_range.clone())
+                    .filter(move |outer_pos| outer_bitmap.index_active(*outer_pos));
+
+                Some((outer_index, outer_pos_iterator))
+            },
+        )
+    }
 }
 
 impl OrderedIndex for InnerPosV2 {
@@ -99,5 +140,45 @@ impl OrderedIndex for InnerPosV2 {
         In: LegMatcher,
     {
         In::inner_pos_iter(range)
+    }
+
+    fn parent_iterator<M, B, Q, In>(
+        market_key: SlotKey<MarketPreimage<M, B, Q>>,
+        range: RangeInclusive<(OuterIndex<Self>, Self)>,
+    ) -> impl Iterator<Item = (OuterIndex<Self>, impl Iterator<Item = Self>)>
+    where
+        M: MarketMarker,
+        B: TokenMarker,
+        Q: TokenMarker,
+        In: LegMatcher,
+    {
+        let mapped_start = (((), range.start().0 .0), range.start().0 .1);
+        let mapped_end = (((), range.end().0 .0), range.end().0 .1);
+
+        OuterPosV2::get_iter::<M, B, Q, In>(market_key, mapped_start..=mapped_end).flat_map(
+            move |((_, outer_bitmap_index), outer_pos_iter)| {
+                let range = range.clone();
+                outer_pos_iter.flat_map(move |outer_pos| {
+                    let outer_index = (outer_bitmap_index, outer_pos);
+
+                    let preimage = BitmapPreimageV2::<M, B, Q, INNER_POS_V2> {
+                        market_key,
+                        outer_index,
+                    };
+
+                    let hash = preimage.hash();
+
+                    let inner_bitmap = hash.load();
+
+                    let inner_pos_range = Self::clamped_range::<In>(&range, outer_index);
+
+                    let inner_pos_iterator =
+                        InnerPosV2::linear_iterator::<In>(inner_pos_range.clone())
+                            .filter(move |inner_pos| inner_bitmap.index_active(*inner_pos));
+
+                    Some((outer_index, inner_pos_iterator))
+                })
+            },
+        )
     }
 }
