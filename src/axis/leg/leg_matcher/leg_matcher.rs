14a3504aac4afe04740e1953e84b924eaafde777
rollchad <138987133+rollchad@users.noreply.github.com>
2026-06-07 00:49:05 +0200

    refactor: Consolidate StoreReader trait bounds on LegReader

--- src/axis/leg/leg_matcher/leg_matcher.rs
+++ src/axis/leg/leg_matcher/leg_matcher.rs
@@ -1,31 +1,12 @@
-use crate::{
-    axis::leg::{
-        leg_constants::LegConstants, leg_coordinates::LegCoordinates, leg_iterator::LegIterator,
-        leg_math::LegMath, leg_quantities::LegQuantities, leg_reader::LegReader,
-        leg_validator::LegValidator, Base, Leg, Quote,
-    },
-    quantities::{BaseLots, QuoteLots},
-    settlement::sender_delta::{SidedSenderDeltaV2, SidedTakeDeltaV2},
-    types::{StoreReader, Tuple},
+use crate::axis::leg::{
+    leg_constants::LegConstants, leg_coordinates::LegCoordinates, leg_iterator::LegIterator,
+    leg_math::LegMath, leg_quantities::LegQuantities, leg_reader::LegReader,
+    leg_validator::LegValidator, Base, Quote,
 };
 
 /// Supertrait for leg operations
 pub trait LegMatcher:
-    LegQuantities
-    + LegMath
-    + LegConstants
-    + LegValidator
-    + LegCoordinates
-    + LegIterator
-    + LegReader
-    + StoreReader<Tuple<QuoteLots, BaseLots, Leg>, Result = <Self::Opposite as LegQuantities>::Lots>
-    + StoreReader<
-        Tuple<SidedSenderDeltaV2<Base>, SidedSenderDeltaV2<Quote>, Leg>,
-        Result = SidedSenderDeltaV2<Self>,
-    > + StoreReader<
-        Tuple<SidedTakeDeltaV2<Base>, SidedTakeDeltaV2<Quote>, Leg>,
-        Result = SidedTakeDeltaV2<Self>,
-    >
+    LegQuantities + LegMath + LegConstants + LegValidator + LegCoordinates + LegIterator + LegReader
 {
 }
 
--- src/axis/leg/leg_reader/leg_reader.rs
+++ src/axis/leg/leg_reader/leg_reader.rs
@@ -1,13 +1,24 @@
 use crate::{
-    axis::leg::{leg_quantities::LegQuantities, Base, Leg, Quote},
-    quantities::{BaseLotsPerBaseUnit, DeltaAtoms, Position, QuoteLotsPerQuoteUnit},
+    axis::leg::{leg_math::LegMath, leg_quantities::LegQuantities, Base, Leg, Quote},
+    quantities::{
+        BaseLots, BaseLotsPerBaseUnit, DeltaAtoms, Position, QuoteLots, QuoteLotsPerQuoteUnit,
+    },
+    settlement::{SidedSenderDeltaV2, SidedTakeDeltaV2},
     types::{StoreReader, Tuple},
 };
 
-pub trait LegReader: LegQuantities
+pub trait LegReader: LegMath
     + StoreReader<Tuple<BaseLotsPerBaseUnit, QuoteLotsPerQuoteUnit, Leg>, Result = Self::LotsPerUnit>
     + StoreReader<Tuple<Position, Position, Leg>, Result = Position>
     + StoreReader<Tuple<DeltaAtoms, DeltaAtoms, Leg>, Result = DeltaAtoms>
+    + StoreReader<Tuple<QuoteLots, BaseLots, Leg>, Result = <Self::Opposite as LegQuantities>::Lots>
+    + StoreReader<
+        Tuple<SidedSenderDeltaV2<Base>, SidedSenderDeltaV2<Quote>, Leg>,
+        Result = SidedSenderDeltaV2<Self>,
+    > + StoreReader<
+        Tuple<SidedTakeDeltaV2<Base>, SidedTakeDeltaV2<Quote>, Leg>,
+        Result = SidedTakeDeltaV2<Self>,
+    >
 {
 }
 
