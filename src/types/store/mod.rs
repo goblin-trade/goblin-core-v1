1820814bd80e436927a574d7b55bf9fc028bb7f4
rollchad <138987133+rollchad@users.noreply.github.com>
2026-06-07 17:51:07 +0200

    refactor: Remove SameTuple trait
    Back to enumeration

--- src/types/store/mod.rs
+++ src/types/store/mod.rs
@@ -1,13 +1,9 @@
 pub mod marker;
-pub mod same_tuple;
-pub mod same_tuple_reader;
 pub mod store_reader;
 pub mod triple;
 pub mod tuple;
 
 pub use marker::*;
-pub use same_tuple::*;
-pub use same_tuple_reader::*;
 pub use store_reader::*;
 pub use triple::*;
 pub use tuple::*;
--- src/types/store/same_tuple_reader.rs
+++
@@ -1,7 +0,0 @@
-use crate::types::same_tuple::SameTuple;
-
-pub trait SameTupleReader<T, K> {
-    fn get(store: &SameTuple<T, K>) -> T;
-    fn get_leg(store: &SameTuple<T, K>) -> &T;
-    fn get_leg_mut(store: &mut SameTuple<T, K>) -> &mut T;
-}
