a4e6abbff70af1a74a9f81dca8aecdc06f17fc80
rollchad <138987133+rollchad@users.noreply.github.com>
2026-06-25 18:03:10 +0200

    refactor: LocalTake sub-struct

--- src/settlement/local_delta_v3/local_delta_v3.rs
+++ src/settlement/local_delta_v3/local_delta_v3.rs
@@ -1,8 +1,18 @@
-use crate::settlement::local_delta_v3::{DeltaLotsPair, LocalDepositsV3, TakeCounterparty};
+use crate::settlement::{
+    local_delta_v3::{local_take::LocalTake, DeltaLotsPair, LocalDepositsV3},
+    ConstZero,
+};
 
 pub struct LocalDeltaV3 {
     pub deposits: LocalDepositsV3,
-    pub take: DeltaLotsPair,
-    pub take_counterparty: TakeCounterparty,
+    pub take: LocalTake,
     pub make: DeltaLotsPair,
+}
+
+impl ConstZero for LocalDeltaV3 {
+    const ZEROED: Self = Self {
+        deposits: LocalDepositsV3::ZEROED,
+        take: LocalTake::ZEROED,
+        make: DeltaLotsPair::ZEROED,
+    };
 }
--- src/settlement/local_delta_v3/mod.rs
+++ src/settlement/local_delta_v3/mod.rs
@@ -1,9 +1,8 @@
 pub mod alias;
 pub mod local_delta_v3;
 pub mod local_deposits_v3;
-pub mod take_counterparty;
+pub mod local_take;
 
 pub use alias::*;
 pub use local_delta_v3::*;
 pub use local_deposits_v3::*;
-pub use take_counterparty::*;
