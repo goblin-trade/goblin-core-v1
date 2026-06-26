d6f1075aa5d0353d369875a6cedc7480c01e3097
rollchad <138987133+rollchad@users.noreply.github.com>
2026-06-26 17:11:47 +0200

    wip: Subtract delta from LocalTake

--- src/settlement/local_delta_v3/local_take/local_take.rs
+++ src/settlement/local_delta_v3/local_take/local_take.rs
@@ -31,6 +31,8 @@ impl LocalTake {
             .get_or_insert_mut(*maker)
             .ok_or(GoblinError::LocalMakerListFull)?;
 
+        // problem- direction
+        // subtract taker_in and add taker_out
         Self::add_for_leg::<In>(&mut self.sender, take_in, base_lot_size, maker_delta_pair)?;
 
         let take_out = In::matching_lots_out(take_in, tick_size, price);
@@ -50,17 +52,22 @@ impl LocalTake {
         base_lot_size: BaseLotsPerBaseUnit,
         maker_delta_pair: &mut DeltaLotsPair,
     ) -> Result<(), GoblinError> {
-        let lots =
+        let unsided_lots = In::decode_matching_lots(matching_lots, base_lot_size).unsided();
+
+        // increase and decrease follows Maker convention
+        // increase = increase resting order, i.e. decrease from store
+
+        let delta_lots =
             DeltaLots::try_from(In::decode_matching_lots(matching_lots, base_lot_size).unsided())?;
 
         let sender_store = In::get_leg_mut(sender);
         *sender_store = sender_store
-            .checked_add(lots)
+            .checked_add(delta_lots)
             .ok_or(GoblinError::DeltaOverflow)?;
 
         let counterparty_store = In::get_leg_mut(maker_delta_pair);
         *counterparty_store = counterparty_store
-            .checked_add(lots)
+            .checked_add(delta_lots)
             .ok_or(GoblinError::DeltaOverflow)?;
 
         Ok(())
