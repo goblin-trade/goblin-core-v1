bcf4a3ce01e3c6875257547f1e36f926cedb585b
rollchad <138987133+rollchad@users.noreply.github.com>
2026-07-15 12:35:28 +0200

    fix: Add max count check when reading CustomERC20Index

--- src/axis/token/token_index/custom_erc20/impl_decodable.rs
+++ src/axis/token/token_index/custom_erc20/impl_decodable.rs
@@ -1,12 +1,18 @@
 use crate::{
-    axis::token::token_index::CustomERC20Index,
+    axis::token::token_index::{CustomERC20Index, MAX_CUSTOM_ERC20_COUNT},
     goblin_error::GoblinError,
     input_processor::{Decodable, DecodeCtx},
+    require,
 };
 
 impl Decodable for CustomERC20Index {
     fn try_decode(ctx: &DecodeCtx) -> Result<Self, GoblinError> {
         let index_raw = u8::try_decode(ctx)? as usize;
+        require!(
+            index_raw <= MAX_CUSTOM_ERC20_COUNT,
+            GoblinError::CustomERC20CountExceeded
+        );
+
         Ok(CustomERC20Index(index_raw))
     }
 }
