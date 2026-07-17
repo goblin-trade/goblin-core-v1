679d0e2eeccf025d47b3a8a246d99b89f2426b58
rollchad <138987133+rollchad@users.noreply.github.com>
2026-07-17 14:34:08 +0200

    refactor: TokenQuantity sub-trait boilerplate

--- src/axis/token/mod.rs
+++ src/axis/token/mod.rs
@@ -1,8 +1,8 @@
 pub mod stub;
 pub mod token;
+pub mod token_marker;
+pub mod token_msg_transfer;
+pub mod token_quantity;
 
 pub use stub::*;
 pub use token::*;
-
-pub mod token_marker;
-pub mod token_msg_transfer;
---
+++ src/axis/token/token_quantity/mod.rs
@@ -0,0 +1,6 @@
+mod custom_erc20;
+mod eth;
+mod hardcoded_erc20;
+pub mod token_quantity;
+
+pub use token_quantity::*;
