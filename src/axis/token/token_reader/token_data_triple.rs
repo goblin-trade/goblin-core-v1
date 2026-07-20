1760849013b8b5bf3f94bc28837a70363eba1a94
rollchad <138987133+rollchad@users.noreply.github.com>
2026-07-20 15:53:34 +0200

    feat: TokenDataList constructor

--- src/axis/token/token_reader/token_data_triple.rs
+++ src/axis/token/token_reader/token_data_triple.rs
@@ -1,5 +1,12 @@
 use crate::{
-    axis::token::{token_list::TokenList, CustomERC20, HardcodedERC20, Token, ETH},
+    axis::token::{
+        token_list::{
+            custom_erc20::CustomERC20List, hardcoded_erc20::HARDCODED_ERC20_LIST, TokenList,
+        },
+        token_marker::TokenData,
+        CustomERC20, HardcodedERC20, Token, ETH,
+    },
+    settlement::ConstZero,
     types::Triple,
 };
 
@@ -9,3 +16,9 @@ pub type TokenDataTriple<'a> = Triple<
     <CustomERC20 as TokenList>::DataList<'a>,
     Token,
 >;
+
+impl<'a> From<CustomERC20List<'a>> for TokenDataTriple<'a> {
+    fn from(value: CustomERC20List<'a>) -> Self {
+        Triple::new(&TokenData::<ETH>::ZEROED, &HARDCODED_ERC20_LIST, value)
+    }
+}
