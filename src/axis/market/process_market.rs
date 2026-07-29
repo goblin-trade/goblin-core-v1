8f1d972f803584577253cc1000cefc9a4ac20e40
rollchad <138987133+rollchad@users.noreply.github.com>
2026-07-29 23:39:17 +0200

    doc: Study type error in locate_market()

--- src/axis/market/process_market.rs
+++ src/axis/market/process_market.rs
@@ -22,6 +22,7 @@ use crate::{
     settlement::Delta,
     types::{Address, StoreReader},
 };
+
 pub fn process_market<'a, MS>(
     msg_sender: &Address,
     ctx: &DecodeCtx,
@@ -39,27 +40,50 @@ where
         delta.local.deposits.decode_and_set::<MS::Pair>(ctx)?;
     }
 
+    // This equals
+    //
+    // * Hardcoded market: HardcodedMarketIndex
+    // * Dynamic market: MarketReadables<(Dynamic, TP)>
+    //
+    // Calling locate_market()
+    //
+    // * hardcoded: use index to lookup market from hardcoded list
+    // * dynamic: return self, i.e. MarketReadables<(Dynamic, TP)>
+    //
+    // TODO can this operate on MS directly?
     let market_locator = <MS::Market as MarketMarker>::MarketLocator::<MS::Pair>::decode_locator(
         ctx,
         token_data_triple,
     )?;
+
+    // How to prove MS = (
+    //     <MS as MarketSpec>::Market,
+    //     <MS as MarketSpec>::Pair,
+    // )
+    let market_readables_v0: &MarketReadables<(
+        <MS as MarketSpec>::Market,
+        <MS as MarketSpec>::Pair,
+    )> = market_locator.locate_market()?;
+
+    // Error
+    // expected &MarketReadables<MS>, found &MarketReadables<(<MS as MarketSpec>::Market, <MS as MarketSpec>::Pair)> (rust-analyzer E0308)
     let market_readables: &MarketReadables<MS> = market_locator.locate_market()?;
 
-    let market_state = &mut market_readables.market_key.load();
+    // let market_state = &mut market_readables.market_key.load();
 
-    let readables = &Readables::<MS> {
-        msg_sender,
-        market_readables,
-    };
+    // let readables = &Readables::<MS> {
+    //     msg_sender,
+    //     market_readables,
+    // };
 
-    let writables = &mut Writables {
-        local_delta: &mut delta.local,
-        market_state,
-    };
+    // let writables = &mut Writables {
+    //     local_delta: &mut delta.local,
+    //     market_state,
+    // };
 
-    // TODO convert to axis- make and take?
-    market_header.execute_takes(ctx, readables, writables)?;
-    market_header.execute_makes(ctx, readables, writables)?;
+    // // TODO convert to axis- make and take?
+    // market_header.execute_takes(ctx, readables, writables)?;
+    // market_header.execute_makes(ctx, readables, writables)?;
 
     // delta.commit_local_delta::<B, Q>(
     //     &market_readables.market.token_index_pair,
