1343dc8b0e54096590d46e9ba23c9729f7dd562f
rollchad <138987133+rollchad@users.noreply.github.com>
2026-08-01 06:43:42 +0200

    doc: Study Decodable trait improvement

--- src/axis/market/common_market/impl_decodable.rs
+++ src/axis/market/common_market/impl_decodable.rs
@@ -1,5 +1,11 @@
+use core::todo;
+
 use crate::{
-    axis::market::{market_spec::MarketSpec, CommonMarket, LotSizePair, TokenIndexPair},
+    axis::{
+        leg::{leg_quantities::LegQuantities, Base, Pair},
+        market::{market_spec::MarketSpec, CommonMarket, LotSizePair, TokenIndexPair, TokenPair},
+        token::token_quantity::TokenQuantity,
+    },
     goblin_error::GoblinError,
     input_processor::{Decodable, DecodablePrimitive, DecodeCtx},
     quantities::QuoteLotsPerBaseUnitPerTick,
@@ -12,24 +18,49 @@ use crate::{
 /// hardcoded markets.
 impl<MS: MarketSpec> Decodable for CommonMarket<MS> {
     fn try_decode(ctx: &DecodeCtx) -> Result<Self, GoblinError> {
-        // Checked decode for token index pair as generics have diferent sizes.
-        // ETH has 0 size.
-        let token_index_pair = TokenIndexPair::<MS::Pair>::try_decode(ctx)?;
+        // TODO improve decoding utilities
+        // In most cases decoding is the same
+        // Check bounds, decode, advance offset
+        //
+        // If implemented on basic types we should have a generic way to get decoded
+        // structs
+        //
+        // Cases
+        // - Simple decode. Construct complex struct from simple units.
+        // - Advanced: interpret bits. This needs custom implementation.
+        let size = core::mem::size_of::<Self>();
 
-        // Batch decode and advance bounds
         require!(
-            ctx.len() >= ctx.offset.get() + 3,
+            ctx.len() >= ctx.offset.get() + size,
             GoblinError::InvalidPayload
         );
-        let lot_size_pair = LotSizePair::decode_unchecked_no_advance(ctx);
-        let tick_size = QuoteLotsPerBaseUnitPerTick::decode_unchecked_no_advance(ctx);
 
-        ctx.advance_offset(3);
+        ctx.advance_offset(size);
 
-        Ok(CommonMarket::new(
-            token_index_pair,
-            lot_size_pair,
-            tick_size,
-        ))
+        todo!()
+
+        // Ok(CommonMarket::)
+
+        // // Checked decode for token index pair as generics have diferent sizes.
+        // // ETH has 0 size.
+        // let token_index_pair = Pair::new(
+        //     <<MS::Pair as TokenPair>::Base as TokenQuantity>::TokenIndex::decode_unchecked(ctx)?,
+        //     <<MS::Pair as TokenPair>::Quote as TokenQuantity>::TokenIndex::try_decode(ctx)?,
+        // );
+
+        // let lot_size_pair = Pair::new(
+        //     <Base as LegQuantities>::LotsPerUnit::decode_unchecked(ctx)?,
+        //     <<MS::Pair as TokenPair>::Quote as TokenQuantity>::TokenIndex::try_decode(ctx)?,
+        // );
+        // let lot_size_pair = LotSizePair::decode_unchecked_no_advance(ctx);
+        // let tick_size = QuoteLotsPerBaseUnitPerTick::decode_unchecked_no_advance(ctx);
+
+        // ctx.advance_offset(3);
+
+        // Ok(CommonMarket::new(
+        //     token_index_pair,
+        //     lot_size_pair,
+        //     tick_size,
+        // ))
     }
 }
