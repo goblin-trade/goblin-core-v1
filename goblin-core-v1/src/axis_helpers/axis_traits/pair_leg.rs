bda21bed1da80b69f0b8f0def5f9704490be1f2e
rollchad <138987133+rollchad@users.noreply.github.com>
2026-08-26 20:22:00 +0200

    refactor: Add Selected field on PairLeg trait

--- goblin-core-v1/src/axis_helpers/axis_traits/pair_leg.rs
+++ goblin-core-v1/src/axis_helpers/axis_traits/pair_leg.rs
@@ -1,5 +1,8 @@
 use crate::{
-    axis::{leg::leg_matcher::LegMatcher, token::token_quantity::TokenQuantity},
+    axis::{
+        leg::leg_matcher::LegMatcher,
+        token::{token_marker::TokenMarker, token_quantity::TokenQuantity},
+    },
     axis_helpers::{LegToToken, TokenPair},
     market::TokenIndexPair,
     settlement::local_delta::LocalDeposits,
@@ -8,7 +11,16 @@ use crate::{
 
 pub trait PairLeg: Clone + Copy {
     type Pair: TokenPair;
-    type Leg: LegMatcher;
+    type Selected: TokenMarker;
+    type Leg: LegMatcher
+        + LegToToken<Self::Pair, Selected = Self::Selected>
+        + StoreReader<
+            TokenIndexPair<Self::Pair>,
+            Result = <Self::Selected as TokenQuantity>::TokenIndex,
+        > + StoreReader<
+            LocalDeposits<Self::Pair>,
+            Result = <Self::Selected as TokenQuantity>::LocalDeposit,
+        >;
 }
 
 impl<TP, In> PairLeg for (TP, In)
@@ -20,5 +32,6 @@ where
         + StoreReader<LocalDeposits<TP>, Result = <In::Selected as TokenQuantity>::LocalDeposit>,
 {
     type Pair = TP;
+    type Selected = In::Selected;
     type Leg = In;
 }
