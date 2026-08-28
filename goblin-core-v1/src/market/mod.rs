4c8eae20e5ef3b447ed5b9d0404413585cd491f9
rollchad <138987133+rollchad@users.noreply.github.com>
2026-08-28 11:50:27 +0200

    refactor: market/pairs submodule

--- goblin-core-v1/src/market/mod.rs
+++ goblin-core-v1/src/market/mod.rs
@@ -1,13 +1,11 @@
 pub mod common_market;
 pub mod header;
-pub mod lot_size_pair;
 pub mod market_readables;
+pub mod pairs;
 pub mod process_market;
-pub mod token_pair;
 
 pub use common_market::*;
 pub use header::*;
-pub use lot_size_pair::*;
 pub use market_readables::*;
+pub use pairs::*;
 pub use process_market::*;
-pub use token_pair::*;
--- goblin-core-v1/src/market/pairs/mod.rs
+++ goblin-core-v1/src/market/pairs/mod.rs
@@ -1,5 +1,7 @@
+pub mod lot_size_pair;
 pub mod token_address_pair;
 pub mod token_index_pair;
 
+pub use lot_size_pair::*;
 pub use token_address_pair::*;
 pub use token_index_pair::*;
