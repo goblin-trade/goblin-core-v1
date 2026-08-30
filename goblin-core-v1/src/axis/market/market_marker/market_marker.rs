4230c148884daa7072f1fa3edb11e7e4f8666811
rollchad <138987133+rollchad@users.noreply.github.com>
2026-08-30 13:51:19 +0200

    refactor: Remove unused MarketHeader::DISCRIMINATOR

--- goblin-core-v1/src/axis/market/market_marker/market_marker.rs
+++ goblin-core-v1/src/axis/market/market_marker/market_marker.rs
@@ -1,25 +1,17 @@
+///! We have 2 market variants
+///!
+///! * Hardcoded market- has hardcoded tokens
+///! * Dynamic market- has dynamic tokens that can be either hardcoded or custom
 use crate::{
     axis::market::{Dynamic, Hardcoded, MarketEnum},
     axis_helpers::AxisMarker,
 };
 
-///! We have 2 market variants
-///!
-///! * Hardcoded market- has hardcoded tokens
-///! * Dynamic market- has dynamic tokens that can be either hardcoded or custom
 pub trait MarketMarker:
-    Sized + Clone + Copy + PartialEq + PartialOrd + AxisMarker<Enum = MarketEnum>
+    Clone + Copy + PartialEq + PartialOrd + AxisMarker<Enum = MarketEnum>
 {
-    /// Discriminator used to hash the market key
-    ///
-    /// TODO unused, remove
-    const DISCRIMINATOR: u8;
 }
 
-impl MarketMarker for Hardcoded {
-    const DISCRIMINATOR: u8 = 3;
-}
+impl MarketMarker for Hardcoded {}
 
-impl MarketMarker for Dynamic {
-    const DISCRIMINATOR: u8 = 4;
-}
+impl MarketMarker for Dynamic {}
