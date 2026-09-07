1ea73263f04a19534d99f6d05c5a48ff7c598723
rollchad <138987133+rollchad@users.noreply.github.com>
2026-09-07 15:41:17 +0200

    wip: Error in eth_hostio::transfer_out()

--- goblin-core-v1/src/axis/token/token_marker/eth/eth.rs
+++ goblin-core-v1/src/axis/token/token_marker/eth/eth.rs
@@ -9,7 +9,8 @@ use crate::{
         HardcodedCaller,
     },
     goblin_error::GoblinError,
-    quantities::UnsidedDeltaAtomsPerLot,
+    hostio::eth_hostio,
+    quantities::{ETHAtoms, UnsidedDeltaAtomsPerLot},
     settlement::UpdateParams,
     state::{IndexedPreimage, SlotKey, StorePreimage},
 };
@@ -28,6 +29,9 @@ impl TokenMarker for ETH {
         // Ok(())
         // TODO fix problem here. Problem in ETH and HardcodedERC20 version
         update_params.update_eth()
+
+        // strange, this works but call on update_params.update_eth() gives error
+        // eth_hostio::transfer_out(&[0u8; 20], &ETHAtoms::default())
     }
 
     fn get_hardcoded_store_hash(
--- goblin-core-v1/src/settlement/global_delta/global_sender/token_settler/update_params/update_params_eth.rs
+++ goblin-core-v1/src/settlement/global_delta/global_sender/token_settler/update_params/update_params_eth.rs
@@ -1,6 +1,7 @@
 use crate::{
     axis::{UpdateMarker, ETH},
     goblin_error::GoblinError,
+    hostio::eth_hostio,
     quantities::ETHAtoms,
     settlement::UpdateParams,
 };
@@ -9,6 +10,20 @@ impl<'a, UM: UpdateMarker> UpdateParams<'a, ETH, UM> {
     pub fn update_eth(&self) -> Result<(), GoblinError> {
         let amount = ETHAtoms::try_from(self.deposit)?;
         let update_address = UM::get_update_address(self.caller_addresses);
-        UM::update_eth(update_address, &amount)
+
+        // error found- passing amount causes call to fail
+        eth_hostio::transfer_out(update_address, &amount)
+        // eth_hostio::transfer_out(update_address, &ETHAtoms::default())
+
+        // let amount = ETHAtoms::try_from(self.deposit)?;
+        // // let update_address = UM::get_update_address(self.caller_addresses);
+
+        // // let update_address = self.caller_addresses.caller;
+
+        // // this causes problem
+        // eth_hostio::transfer_out(&[0u8; 20], &amount)
+        // eth_hostio::transfer_out(update_address, &amount)
+        // Ok(())
+        // UM::update_eth(update_address, &amount)
     }
 }
