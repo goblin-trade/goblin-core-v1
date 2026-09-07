79e596aa3422b6a369a69cecc1b75de96d6cd690
rollchad <138987133+rollchad@users.noreply.github.com>
2026-09-07 20:01:02 +0200

    refactor: Do not use two separate keccak crates
    Use keccak_const in goblin-hostio

--- goblin-hostio/src/hostio_unsafe/tests/test_hooks/keccak.rs
+++ goblin-hostio/src/hostio_unsafe/tests/test_hooks/keccak.rs
@@ -1,13 +1,10 @@
 extern crate alloc;
-use tiny_keccak::{Hasher, Keccak};
+use keccak_const::Keccak256;
 
 #[no_mangle]
 pub unsafe extern "C" fn native_keccak256(bytes: *const u8, len: usize, output: *mut u8) {
     let input_slice = core::slice::from_raw_parts(bytes, len);
-    let mut hasher = Keccak::v256();
-    hasher.update(input_slice);
-    let mut result = [0u8; 32];
-    hasher.finalize(&mut result);
+    let result = Keccak256::new().update(input_slice).finalize();
     let output_slice = core::slice::from_raw_parts_mut(output, 32);
     output_slice.copy_from_slice(&result);
 }
