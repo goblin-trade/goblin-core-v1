385315aefaa0d86d32c322aca043cbe3d71eacc8
rollchad <138987133+rollchad@users.noreply.github.com>
2026-09-14 12:50:43 +0200

    wip: Combine i64 and u64 aliases

--- goblin-core/src/quantities/quantity/delta/alias.rs
+++ goblin-core/src/quantities/quantity/delta/alias.rs
@@ -12,11 +12,3 @@ pub type BaseDeltaLots =
     Quantity<Dim<SidedDim<Base, P1, Z0, Z0>, SidedDim<Quote, Z0, Z0, Z0>, Z0>, i64>;
 pub type QuoteDeltaLots =
     Quantity<Dim<SidedDim<Base, Z0, Z0, Z0>, SidedDim<Quote, P1, Z0, Z0>, Z0>, i64>;
-
-////// Unsided delta
-pub type UnsidedDeltaAtoms = Unsided<Z0, Z0, P1, i64>;
-pub type UnsidedDeltaLots = Unsided<P1, Z0, Z0, i64>;
-
-pub type UnsidedDeltaAtomsPerUnit = Unsided<Z0, N1, P1, i64>;
-pub type UnsidedDeltaAtomsPerLot = Unsided<N1, Z0, P1, i64>;
-pub type UnsidedDeltaLotsPerUnit = Unsided<P1, N1, Z0, i64>;
